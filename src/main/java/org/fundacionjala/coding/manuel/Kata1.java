65edafc7abd461eaf32a54484442b6490d0affb8
manu863018 <Manuel.Valdez@fundacion-jala.org>
2018-02-23 01:00:12 +0000

    Commit Kata1

--- src/main/java/org/fundacionjala/coding/manuel/Kata1.java
+++ src/main/java/org/fundacionjala/coding/manuel/Kata1.java
@@ -4,4 +4,38 @@ package org.fundacionjala.coding.manuel;
  * Created by Administrator on 2/22/2018.
  */
 public class Kata1 {
+    private String sentence;
+    private static final int MAX_SIZE = 5;
+
+    /**
+     *@param w Constructor by default.
+     */
+    public Kata1(String w) {
+        this.sentence = w;
+    }
+    /**
+     *@return Method to spin the sentence or word.
+     */
+    public String spinWord() {
+        String[] words = sentence.split(" ");
+        String newSentece = "";
+        StringBuilder newWord;
+        StringBuffer buffer = new StringBuffer();
+        for (String singleWord : words) {
+            newWord = new StringBuilder(singleWord);
+
+            if (singleWord.length() >= MAX_SIZE) {
+                buffer.append(newWord.reverse());
+                //newSentece = newSentece + ;
+            } else {
+                buffer.append(singleWord);
+                //newSentece = newSentece + ;
+            }
+            buffer.append(' ');
+            //newSentece = newSentece + " ";
+        }
+        newSentece = buffer.toString();
+        newSentece = newSentece.trim();
+        return newSentece;
+    }
 }
--- src/test/java/org/fundacionjala/coding/manuel/Kata1Test.java
+++ src/test/java/org/fundacionjala/coding/manuel/Kata1Test.java
@@ -1,10 +1,38 @@
 package org.fundacionjala.coding.manuel;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Administrator on 2/22/2018.
  */
 public class Kata1Test {
 
+    /**
+     *First test.
+     */
+    @Test
+    public void spinASentence1() {
+        Kata1 kata1 = new Kata1("Hey fellow warriors");
+        assertEquals("Hey wollef sroirraw", kata1.spinWord());
+    }
+
+    /**
+     *Second test.
+     */
+    @Test
+    public void spinASentence2() {
+        Kata1 kata1 = new Kata1("This is a test");
+        assertEquals("This is a test", kata1.spinWord());
+    }
+
+    /**
+     *Third test.
+     */
+    @Test
+    public void spinASentence3() {
+        Kata1 kata1 = new Kata1("This is another test");
+        assertEquals("This is rehtona test", kata1.spinWord());
+    }
 }
