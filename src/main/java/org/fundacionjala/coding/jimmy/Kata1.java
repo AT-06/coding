1740be66c7992d34a5c870fb8d444dedbce49246
JimmyRomero <jimmy.romero@fundacion-jala.org>
2018-02-22 20:40:23 -0400

    Kata1 developed

---
+++ src/main/java/org/fundacionjala/coding/jimmy/Kata1.java
@@ -0,0 +1,10 @@
+package org.fundacionjala.coding.jimmy;
+
+public class Kata1 {
+    String word;
+    public void spinWord(String wordsToSpin){
+        this.word = wordsToSpin;
+
+        String toCompare = word.split( );
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/jimmy/Kata1Test.java
@@ -0,0 +1,17 @@
+package org.fundacionjala.coding.jimmy;
+
+import org.junit.Test;
+
+public class Kata1Test {
+    @Test
+    public void test1(){
+
+    }
+
+    spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+
+    spinWords( "This is a test") => returns "This is a test"
+
+    spinWords( "This is another test" )=> returns "This is rehtona test"
+
+}
