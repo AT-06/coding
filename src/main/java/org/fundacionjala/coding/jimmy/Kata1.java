9031e1b4d630057b2ecb1aa67f2156cf51faf9d7
JimmyRomero <jimmy.romero@fundacion-jala.org>
2018-02-22 20:47:53 -0400

    Kata1 developed

--- src/main/java/org/fundacionjala/coding/jimmy/Kata1.java
+++ src/main/java/org/fundacionjala/coding/jimmy/Kata1.java
@@ -1,10 +1,29 @@
 package org.fundacionjala.coding.jimmy;
 
+/**
+ * This class contains a method which is going to spin a string whether is greater than 5 in length.
+ */
 public class Kata1 {
-    String word;
-    public void spinWord(String wordsToSpin){
-        this.word = wordsToSpin;
 
-        String toCompare = word.split( );
+    public static final int THRESHOLD = 5;
+
+    /**
+     * @param wordsToSpin is a method for spin a word greater than 5 in length.
+     * @return a word spinned if is greater than 5 in length.
+     */
+    public String spinWords(String wordsToSpin) {
+        String[] arrayWords = wordsToSpin.split(" ");
+        String string; String space = " ";
+        StringBuffer buffer = new StringBuffer();
+        for (String eachWord : arrayWords) {
+            if (eachWord.length() >= THRESHOLD) {
+                buffer.append(new StringBuilder(eachWord).reverse());
+            } else {
+                buffer.append(eachWord);
+            }
+            buffer.append(space);
+        }
+        string = buffer.toString();
+        return string.trim();
     }
 }
--- src/test/java/org/fundacionjala/coding/jimmy/Kata1Test.java
+++ src/test/java/org/fundacionjala/coding/jimmy/Kata1Test.java
@@ -2,16 +2,43 @@ package org.fundacionjala.coding.jimmy;
 
 import org.junit.Test;
 
-public class Kata1Test {
-    @Test
-    public void test1(){
+import static org.junit.Assert.assertEquals;
 
+/**
+ * These test are designed to control if a word is greater than five in length.
+ */
+public class Kata1Test {
+
+    /**
+     * This test verifies the spinWords method.
+     */
+    @Test
+    public void test1() {
+        Kata1 kata1 = new Kata1();
+
+        String word = "Hey fellow warriors";
+        assertEquals("Hey wollef sroirraw", kata1.spinWords(word));
     }
 
-    spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+    /**
+     * This test verifies the spinWords method.
+     */
+    @Test
+    public void test2() {
+        Kata1 kata1 = new Kata1();
 
-    spinWords( "This is a test") => returns "This is a test"
+        String word = "This is a test";
+        assertEquals("This is a test", kata1.spinWords(word));
+    }
 
-    spinWords( "This is another test" )=> returns "This is rehtona test"
+    /**
+     * This test verifies the spinWords method.
+     */
+    @Test
+    public void test3() {
+        Kata1 kata1 = new Kata1();
 
+        String word = "This is another test";
+        assertEquals("This is rehtona test", kata1.spinWords(word));
+    }
 }
