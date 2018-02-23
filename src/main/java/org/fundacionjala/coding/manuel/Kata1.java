a1ef33a8cc63549bce8308e07ee887dd772536de
manu863018 <Manuel.Valdez@fundacion-jala.org>
2018-02-23 00:56:01 +0000

    Committing Kata1 resolved

---
+++ src/main/java/org/fundacionjala/coding/manuel/Kata1.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding.manuel;
+
+/**
+ * Created by Administrator on 2/22/2018.
+ */
+public class Kata1 {
+}
---
+++ src/test/java/org/fundacionjala/coding/manuel/Kata1Test.java
@@ -0,0 +1,10 @@
+package org.fundacionjala.coding.manuel;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2/22/2018.
+ */
+public class Kata1Test {
+
+}
