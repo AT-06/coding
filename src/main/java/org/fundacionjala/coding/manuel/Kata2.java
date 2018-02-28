f52307df92e777ab992d4e48ac78131f13f73237
manu863018 <Manuel.Valdez@fundacion-jala.org>
2018-02-28 12:50:31 +0000

    Kata 2 second solution

--- src/main/java/org/fundacionjala/coding/manuel/Kata2.java
+++ src/main/java/org/fundacionjala/coding/manuel/Kata2.java
@@ -1,7 +1,46 @@
 package org.fundacionjala.coding.manuel;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+
 /**
  * Created by Administrator on 2/23/2018.
  */
 public class Kata2 {
+    private String sentence;
+    private static final int MAX_LENGHT = 3;
+
+    /**
+     * Created by Administrator on 2/23/2018.
+     * @param sentence es la oracion original.
+     */
+    public Kata2(String sentence) {
+        this.sentence = sentence;
+    }
+
+    /**
+     * @return string que es la oracion ordenada.
+     */
+    public String sortInnerContent() {
+        String[] words = sentence.split(" ");
+        StringBuffer newSentece = new StringBuffer();
+
+        for (String singleWord : words) {
+            String[] letters = singleWord.split("");
+            if (singleWord.length() > MAX_LENGHT) {
+                Arrays.sort(letters, 1, singleWord.length() - 1, Collections.reverseOrder());
+                for (String let : letters) {
+                    newSentece.append(let);
+                }
+            } else {
+                newSentece.append(singleWord);
+            }
+            newSentece.append(' ');
+        }
+        String returnSentence = newSentece.toString();
+        returnSentence = returnSentence.trim();
+        return  returnSentence;
+    }
 }
+
--- src/test/java/org/fundacionjala/coding/manuel/Kata2Test.java
+++ src/test/java/org/fundacionjala/coding/manuel/Kata2Test.java
@@ -1,10 +1,46 @@
 package org.fundacionjala.coding.manuel;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Administrator on 2/23/2018.
  */
 public class Kata2Test {
+    /**
+     * Verificar que las palabras de una oracion hayan sido ordenadas.
+     */
+    @Test
+    public void aStringInnerContentIsSort1() {
+        Kata2 kata2 = new Kata2("sort the inner content in descending order");
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr", kata2.sortInnerContent());
+    }
+
+    /**
+     * Verificar que las palabras de una oracion hayan sido ordenadas.
+     */
+    @Test
+    public void aStringInnerContentIsSort2() {
+        Kata2 kata2 = new Kata2("wait for me");
+        assertEquals("wiat for me", kata2.sortInnerContent());
+    }
+
+    /**
+     * Verificar que las palabras de una oracion hayan sido ordenadas.
+     */
+    @Test
+    public void aStringInnerContentIsSort3() {
+        Kata2 kata2 = new Kata2("this kata is easy");
+        assertEquals("tihs ktaa is esay", kata2.sortInnerContent());
+    }
+
+    /**
+     * Verificar que las palabras de una oracion hayan sido ordenadas.
+     */
+    @Test
+    public void aStringInnerContentIsSort4() {
+        Kata2 kata2 = new Kata2("I am");
+        assertEquals("I am", kata2.sortInnerContent());
+    }
 
 }
