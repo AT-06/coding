78d80ac315c7cf7fded418d903e58fcb6c784d09
Omar Huanca <omar.huanca@fundacion-jala.org>
2018-02-28 18:28:01 -0400

    Kata2 completed

--- src/main/java/org/fundacionjala/coding/omar/Kata2.java
+++ src/main/java/org/fundacionjala/coding/omar/Kata2.java
@@ -2,6 +2,7 @@ package org.fundacionjala.coding.omar;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.StringJoiner;
 
 /**
  * Powered by Omar Limbert Huanca Sanchez - AT06
@@ -17,8 +18,7 @@ public class Kata2 {
     public String sortInDescendingOrder(String words) {
 
         String[] wordsForSort = words.split(" ");
-        StringBuffer buffer = new StringBuffer();
-        String space = " ";
+        StringJoiner buffer = new StringJoiner(" ");
 
         for (String word : wordsForSort) {
 
@@ -26,14 +26,11 @@ public class Kata2 {
             if (aux.length != 1) {
                 Arrays.sort(aux, 1, aux.length - 1, Collections.reverseOrder());
             }
-            for (String c : aux) {
-                buffer.append(c);
-            }
-            buffer.append(space);
+            buffer.add(String.join("", aux));
+
         }
-        System.out.println(buffer);
 
 
-        return buffer.toString().trim();
+        return buffer.toString();
     }
 }
