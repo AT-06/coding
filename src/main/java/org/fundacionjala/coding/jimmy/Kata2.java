8df38cdceafe06c130b7254ed4595d06ea225b11
JimmyRomero <jimmy.romero@fundacion-jala.org>
2018-02-28 03:08:36 -0400

    Kata2 developed

--- src/main/java/org/fundacionjala/coding/jimmy/Kata2.java
+++ src/main/java/org/fundacionjala/coding/jimmy/Kata2.java
@@ -1,4 +1,41 @@
 package org.fundacionjala.coding.jimmy;
 
-public class Kata2 {
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * This kata was performed by Jimmy Romero.
+ */
+public final class Kata2 {
+
+    /**
+     * Constructor method.
+     */
+    private Kata2() {
+    }
+
+    private static final int MINIMUN_LENGTH_OF_WORD = 2;
+
+    /**
+     * @param stringWords is the complete string which is going to change its inner.
+     * @return the inner of the word sorted.
+     */
+    public static String sortInnerContent(String stringWords) {
+        return Arrays.stream(stringWords.split(" ")).map(w -> sortWord(w)).collect(Collectors.joining(" "));
+    }
+
+    /**
+     * @param word is the independent word which is going to change its inner.
+     * @return a word with its inner changed.
+     */
+    private static String sortWord(String word) {
+        if (word.length() < MINIMUN_LENGTH_OF_WORD) {
+            return word;
+        } else {
+            char[] innerWord = word.substring(1, word.length() - 1).toCharArray();
+            Arrays.sort(innerWord);
+            String reverseInnerWord = new StringBuilder(String.valueOf(innerWord)).reverse().toString();
+            return word.charAt(0) + reverseInnerWord + word.charAt(word.length() - 1);
+        }
+    }
 }
--- src/test/java/org/fundacionjala/coding/jimmy/Kata2Test.java
+++ src/test/java/org/fundacionjala/coding/jimmy/Kata2Test.java
@@ -1,5 +1,47 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.jimmy;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test class created by Jimmy Romero.
+ */
 public class Kata2Test {
 
+    /**
+     * This test is performed in order to compare the expected and actual result.
+     */
+    @Test
+    public void test1() {
+        String stringWords = "sort the inner content in descending order";
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr", Kata2.sortInnerContent(stringWords));
+    }
+
+    /**
+     * This test is performed in order to compare the expected and actual result.
+     */
+    @Test
+    public void test2() {
+        String stringWords = "wait for me";
+        assertEquals("wiat for me", Kata2.sortInnerContent(stringWords));
+    }
+
+    /**
+     * This test is performed in order to compare the expected and actual result.
+     */
+    @Test
+    public void test3() {
+        String stringWords = "this kata is easy";
+        assertEquals("tihs ktaa is esay", Kata2.sortInnerContent(stringWords));
+    }
+
+    /**
+     * This test is performed in order to compare the expected and actual result.
+     */
+    @Test
+    public void test4() {
+        String stringWords = "I am";
+        assertEquals("I am", Kata2.sortInnerContent(stringWords));
+    }
 }
