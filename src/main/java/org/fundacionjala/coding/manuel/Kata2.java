d9d038f4b60b6dcd6f81e77f55ac227d7bd5e63f
manu863018 <Manuel.Valdez@fundacion-jala.org>
2018-02-28 12:46:13 +0000

    Kata 2 first solution

---
+++ src/main/java/org/fundacionjala/coding/manuel/Kata2.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding.manuel;
+
+/**
+ * Created by Administrator on 2/23/2018.
+ */
+public class Kata2 {
+}
---
+++ src/test/java/org/fundacionjala/coding/manuel/Kata2Test.java
@@ -0,0 +1,10 @@
+package org.fundacionjala.coding.manuel;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2/23/2018.
+ */
+public class Kata2Test {
+
+}
