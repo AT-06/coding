c1e2cc217fffa309a6c9b46ee9a3a5d8f0433598
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-03-01 19:17:15 -0400

    kata 4 solved 2

--- src/main/java/org/fundacionjala/coding/maria/Kata4.java
+++ src/main/java/org/fundacionjala/coding/maria/Kata4.java
@@ -1,4 +1,24 @@
 package org.fundacionjala.coding.maria;
 
+/**
+ * Kata 4.
+ * Sum number digits.
+ */
 public class Kata4 {
+    private static final int DIV = 10;
+    /**
+     * @param num numero
+     * @return total suma
+     */
+    public int sumDigits(int num) {
+        while (num / DIV > 0) {
+            String stringNum = Integer.toString(num);
+            int[] digits = stringNum.chars().map(Character::getNumericValue).toArray();
+            num = 0;
+            for (int digit : digits) {
+                num += digit;
+            }
+        }
+        return num;
+    }
 }
--- src/test/java/org/fundacionjala/coding/maria/Kata4Test.java
+++ src/test/java/org/fundacionjala/coding/maria/Kata4Test.java
@@ -1,5 +1,33 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.maria;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test kata4.
+ */
 public class Kata4Test {
+    /**
+     * Test sum digits.
+     */
+    @Test
+    public void testSumDigits() {
+
+        final int case1 = 942;
+        final int case2 = 132189;
+        final int case3 = 493193;
+
+        final int exp1 = 6;
+        final int exp2 = 6;
+        final int exp3 = 2;
+
+
+        Kata4 num = new Kata4();
+        assertEquals(exp1, num.sumDigits(case1));
+        assertEquals(exp2, num.sumDigits(case2));
+        assertEquals(exp3, num.sumDigits(case3));
+
+    }
 
 }
