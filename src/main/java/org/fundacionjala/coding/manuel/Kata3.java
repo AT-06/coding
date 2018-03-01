72d77d76ca2d088ff2966cb9a64652783c5fd7c2
manu863018 <Manuel.Valdez@fundacion-jala.org>
2018-03-01 21:40:23 +0000

    Kata3 first solution

--- src/main/java/org/fundacionjala/coding/manuel/Kata3.java
+++ src/main/java/org/fundacionjala/coding/manuel/Kata3.java
@@ -4,4 +4,29 @@ package org.fundacionjala.coding.manuel;
  * Created by Administrator on 3/1/2018.
  */
 public class Kata3 {
+    private static final int MAX_DIGITS = 9;
+    private static final int MOD_NUMBER = 10;
+
+    /**
+     * Method to know how many persistance a number has.
+     * @param number s.
+     * @return returns the amount of persistance.
+     */
+    public int persistence(int number) {
+        int copiedNumber = number;
+        int multipliedNumber;
+        int count = 0;
+        while (copiedNumber > MAX_DIGITS) {
+            multipliedNumber = 1;
+            while (copiedNumber > MAX_DIGITS) {
+                multipliedNumber *= copiedNumber % MOD_NUMBER;
+                copiedNumber /= MOD_NUMBER;
+            }
+            multipliedNumber *= copiedNumber;
+            copiedNumber = multipliedNumber;
+            count++;
+        }
+        return count;
+    }
+
 }
--- src/test/java/org/fundacionjala/coding/manuel/Kata3Test.java
+++ src/test/java/org/fundacionjala/coding/manuel/Kata3Test.java
@@ -1,7 +1,35 @@
 package org.fundacionjala.coding.manuel;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 /**
  * Created by Administrator on 3/1/2018.
  */
 public class Kata3Test {
+
+    private static final int TEST_NUMBER_0 = 0;
+    private static final int TEST_NUMBER_2 = 2;
+    private static final int TEST_NUMBER_3 = 3;
+    private static final int TEST_NUMBER_4 = 4;
+    private static final int TEST_NUMBER_25 = 25;
+    private static final int TEST_NUMBER_39 = 39;
+    private static final int TEST_NUMBER_444 = 444;
+    private static final int TEST_NUMBER_999 = 999;
+
+    /**
+     * test the persitance method..
+     */
+    @Test
+    public void testPersistenceKata3() {
+        Kata3 kata3 = new Kata3();
+        System.out.println("****** Basic Tests ******");
+        assertEquals(TEST_NUMBER_3, kata3.persistence(TEST_NUMBER_39));
+        assertEquals(TEST_NUMBER_0, kata3.persistence(TEST_NUMBER_4));
+        assertEquals(TEST_NUMBER_2, kata3.persistence(TEST_NUMBER_25));
+        assertEquals(TEST_NUMBER_4, kata3.persistence(TEST_NUMBER_999));
+        assertEquals(TEST_NUMBER_3, kata3.persistence(TEST_NUMBER_444));
+    }
+
 }
+
+
