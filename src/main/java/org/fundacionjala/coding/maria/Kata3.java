d39cd83a3865ab83592056a076de529b8218055a
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-03-01 17:34:23 -0400

    kata 3 solved 2

--- src/main/java/org/fundacionjala/coding/maria/Kata3.java
+++ src/main/java/org/fundacionjala/coding/maria/Kata3.java
@@ -1,4 +1,26 @@
 package org.fundacionjala.coding.maria;
 
+/**
+ * test.
+ */
 public class Kata3 {
+
+    private static final int LIM = 10;
+    /**
+     * @param num numero
+     * @return total persistent
+     */
+    public int persistence(int num) {
+
+        int cont = 0;
+        while (num / LIM > 0) {
+            int[] digits = Integer.toString(num).chars().map(c -> c -= '0').toArray();
+            num = 1;
+            for (int tam : digits) {
+                num = num * tam;
+            }
+            cont++;
+        }
+        return cont;
+    }
 }
--- src/test/java/org/fundacionjala/coding/maria/Kata3Test.java
+++ src/test/java/org/fundacionjala/coding/maria/Kata3Test.java
@@ -1,5 +1,51 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.maria;
 
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test kata 3.
+ */
 public class Kata3Test {
+    public static final int INI1 = 39;
+    public static final int FIN1 = 3;
+    public static final int INI2 = 25;
+    public static final int FIN2 = 2;
+    public static final int INI3 = 444;
+    public static final int FIN3 = 3;
+    public static final int INI4 = 4;
+    public static final int FIN4 = 0;
+    public static final int INI5 = 999;
+    public static final int FIN5 = 4;
+
+    /**
+     * Test testpersistentBuggerOne.
+     */
+    @Test
+    public void testpersistentBuggerOne() {
+        Kata3 persist = new Kata3();
+
+        assertEquals(FIN1, persist.persistence(INI1));
+        assertEquals(FIN2, persist.persistence(INI2));
+        assertEquals(FIN3, persist.persistence(INI3));
+    }
+    /**
+     * Test testpersistentBuggerTwo.
+     */
+    @Test
+    public void testpersistentBuggerTwo() {
+        Kata3 persist = new Kata3();
+        assertEquals(FIN4, persist.persistence(INI4));
+    }
+    /**
+     * Test testpersistentBuggerThree.
+     */
+    @Test
+    public void testpersistentBuggerThree() {
+        Kata3 persist = new Kata3();
+        assertEquals(FIN5, persist.persistence(INI5));
+    }
 
 }
