22a651d172dfd3ff1ae39d619f30d6a4a29fff41
pipo411 <Ariel.Gonzales@fundacion-jala.org>
2018-03-01 17:54:53 -0400

    Kata3 using String and convert Int.

--- src/main/java/org/fundacionjala/coding/ariel/Kata3.java
+++ src/main/java/org/fundacionjala/coding/ariel/Kata3.java
@@ -1,4 +1,27 @@
 package org.fundacionjala.coding.ariel;
-
+/**
+ * Made For Ariel.
+ */
 public class Kata3 {
+
+    /**
+     * @param num Any int num
+     * @return A int num, but apply the persistentBurgger..
+     */
+    public int persistentBurgger(int num) {
+        int product = 1;
+        int i = 0;
+        String numText = String.valueOf(num);
+        String[] numbers = numText.split("");
+        while (numbers.length != 1) {
+            for (String n : numbers) {
+                product = product * Integer.parseInt(n);
+            }
+            numText = String.valueOf(product);
+            product = 1;
+            numbers = numText.split("");
+            i++;
+        }
+        return i;
+    }
 }
--- src/test/java/org/fundacionjala/coding/ariel/Kata3Test.java
+++ src/test/java/org/fundacionjala/coding/ariel/Kata3Test.java
@@ -1,5 +1,37 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.ariel;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * It test check five conditions..
+ */
 public class Kata3Test {
+
+    public static final int R1 = 3;
+    public static final int R2 = 0;
+    public static final int R3 = 2;
+    public static final int R4 = 4;
+    public static final int R5 = 3;
+    public static final int INPUT1 = 39;
+    public static final int INPUT2 = 4;
+    public static final int INPUT3 = 25;
+    public static final int INPUT4 = 999;
+    public static final int INPUT5 = 444;
+
+    /**
+     * It test made the five conditions.
+     */
+    @Test
+    public void basicTests() {
+        System.out.println("****** Basic Tests ******");
+        Kata3 katita = new Kata3();
+        assertEquals(R1, katita.persistentBurgger(INPUT1));
+        assertEquals(R2, katita.persistentBurgger(INPUT2));
+        assertEquals(R3, katita.persistentBurgger(INPUT3));
+        assertEquals(R4, katita.persistentBurgger(INPUT4));
+        assertEquals(R5, katita.persistentBurgger(INPUT5));
+    }
 
 }
