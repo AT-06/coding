3061c2b96751f2a5f76daca3248c7fa407aa7ef0
Daniel Caballero <daniel.caballero@fundacion-jala.org>
2018-03-04 10:03:39 -0400

    Fixed Kata 4

--- src/test/java/org/fundacionjala/coding/daniel/Kata4Test.java
+++ src/test/java/org/fundacionjala/coding/daniel/Kata4Test.java
@@ -32,10 +32,16 @@ public class Kata4Test {
         final int number3 = 132189;
         final int expected4 = 2;
         final int number4 = 493193;
+        final int expected5 = 0;
+        final int number5 = 0;
+        final int expected6 = 9;
+        final int number6 = 9;
 
         assertEquals(expected1, kata.digitalRoot(number1));
         assertEquals(expected2, kata.digitalRoot(number2));
         assertEquals(expected3, kata.digitalRoot(number3));
         assertEquals(expected4, kata.digitalRoot(number4));
+        assertEquals(expected5, kata.digitalRoot(number5));
+        assertEquals(expected6, kata.digitalRoot(number6));
     }
 }
