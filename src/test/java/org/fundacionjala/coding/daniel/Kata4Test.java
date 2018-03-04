f00150708d4adead1de1f28f2f3c8e2d66675fef
Daniel Caballero <daniel.caballero@fundacion-jala.org>
2018-03-03 23:13:48 -0400

    Added Kata 4

---
+++ src/test/java/org/fundacionjala/coding/daniel/Kata4Test.java
@@ -0,0 +1,41 @@
+package org.fundacionjala.coding.daniel;
+
+import static junit.framework.TestCase.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * A class for testing Kata4 class.
+ */
+public class Kata4Test {
+    private Kata4 kata;
+
+    /**
+     * Initializes variable kata.
+     */
+    @Before
+    public void init() {
+        kata = new Kata4();
+    }
+
+    /**
+     * unit testing of digitalRoot method.
+     */
+    @Test
+    public void tests() {
+        final int expected1 = 7;
+        final int number1 = 16;
+        final int expected2 = 6;
+        final int number2 = 942;
+        final int expected3 = 6;
+        final int number3 = 132189;
+        final int expected4 = 2;
+        final int number4 = 493193;
+
+        assertEquals(expected1, kata.digitalRoot(number1));
+        assertEquals(expected2, kata.digitalRoot(number2));
+        assertEquals(expected3, kata.digitalRoot(number3));
+        assertEquals(expected4, kata.digitalRoot(number4));
+    }
+}
