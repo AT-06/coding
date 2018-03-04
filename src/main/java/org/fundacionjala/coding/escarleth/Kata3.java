123d563aeb5135219763be581162e4f84b663dbc
EscarlethFatima <Escarleth.Ledezma@fundacion-jala.org>
2018-03-04 12:39:07 -0800

    Escarleth kata3

---
+++ src/main/java/org/fundacionjala/coding/escarleth/Kata3.java
@@ -0,0 +1,29 @@
+package org.fundacionjala.coding.escarleth;
+
+/**
+ * Powered by Escarleth Ledezma Q.
+ * Kata3 is a class that contains a persistentBugger method
+ */
+public class Kata3 {
+    private static final int SUM = 1;
+    private static final int LIMIT = 1;
+
+    /**
+     * @param numberToPersist is a number that will find
+     *                        a numberPersistence(multiply every digit
+     *                        until the number of digits is one).
+     * @return result
+     */
+    public int persistentBugger(int numberToPersist) {
+        String[] numbers = String.valueOf(numberToPersist).split("");
+        int valueMultiplied = 1;
+        if (numbers.length == LIMIT) {
+            return 0;
+        } else {
+            for (String number : numbers) {
+                valueMultiplied *= Integer.parseInt(number);
+            }
+            return SUM + persistentBugger(valueMultiplied);
+        }
+    }
+}
