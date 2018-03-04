f0033fc16dfa9108653ed66852eceea8a252cb06
EscarlethFatima <Escarleth.Ledezma@fundacion-jala.org>
2018-03-04 12:57:23 -0800

    Escarleth kata4

---
+++ src/main/java/org/fundacionjala/coding/escarleth/Kata4.java
@@ -0,0 +1,28 @@
+package org.fundacionjala.coding.escarleth;
+
+/**
+ * Powered by Escarleth Ledezma Q.
+ * Kata4 is a class that contains a digitalRoot method
+ */
+public class Kata4 {
+    private static final int DIVDIGIT = 10;
+    private static final int LIMIT = 0;
+
+    /**
+     * @param numberToSum is a number that will find a digitalRoot
+     *                    (recursive method that sum every digit until the numberToSum is less to 10)
+     * @return result
+     */
+    public int digitalRoot(int numberToSum) {
+        int digitsSum = 0;
+        if (numberToSum < DIVDIGIT) {
+            return numberToSum;
+        } else {
+            while (numberToSum != LIMIT) {
+                digitsSum += numberToSum % DIVDIGIT;
+                numberToSum /= DIVDIGIT;
+            }
+            return digitalRoot(digitsSum);
+        }
+    }
+}
