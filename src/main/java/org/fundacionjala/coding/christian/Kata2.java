593274c8ae1d24215bc187ff1d1c0170fb38758a
Christian Galarza <Christian.Galarza@fundacion-jala.org>
2018-03-05 08:35:54 -0400

    Kata2 corrected

--- src/main/java/org/fundacionjala/coding/christian/Kata2.java
+++ src/main/java/org/fundacionjala/coding/christian/Kata2.java
@@ -4,31 +4,33 @@ package org.fundacionjala.coding.christian;
  */
 import java.util.Arrays;
 import  java.util.Collections;
+import java.util.StringJoiner;
 
 /**
  *
  */
 public class Kata2 {
     private static final int LIST_LENGTH = 3;
+
     /**
      * @param words this is for Word
-     * @return tag for 'words'.
-     */
+     * @return tag for 'words'
+     * */
     public String word(String words) {
         String[] wordsList = words.split(" ");
-        StringBuilder build = new StringBuilder();
+        StringJoiner joiner = new StringJoiner("");
         for (int i = 0; i < wordsList.length; i++) {
             String[] changedPosition = wordsList[i].split("");
             if (changedPosition.length > LIST_LENGTH) {
                 Arrays.sort(changedPosition, 1, changedPosition.length - 1, Collections.reverseOrder());
             }
             for (String letter : changedPosition) {
-                build.append(letter);
+                joiner.add(letter);
             }
             if (i < wordsList.length - 1) {
-                build.append(' ');
+                StringJoiner add = joiner.add(" ");
             }
         }
-        return build.toString();
+        return joiner.toString();
     }
 }
