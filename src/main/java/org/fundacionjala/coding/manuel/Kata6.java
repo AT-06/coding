efde4d1a483c692c9c0ea8ee59e6efd10fd096f4
manu863018 <Manuel.Valdez@fundacion-jala.org>
2018-03-05 19:53:29 +0000

    Kata6 solution first attempt

---
+++ src/main/java/org/fundacionjala/coding/manuel/Kata6.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding.manuel;
+
+/**
+ * Created by Administrator on 3/5/2018.
+ */
+public class Kata6 {
+}
---
+++ src/test/java/org/fundacionjala/coding/manuel/Kata6Test.java
@@ -0,0 +1,10 @@
+package org.fundacionjala.coding.manuel;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 3/5/2018.
+ */
+public class Kata6Test {
+
+}
