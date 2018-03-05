5899e3c8ab9a9e0948d371863cea28278f0abbb2
pipo411 <35770134+pipo411@users.noreply.github.com>
2018-03-05 04:45:51 -0400

    Ariel Kata1 (#12)
    * kata1 rot a words that are more than five characters.
    
    * kata2, using the function string join, sort, slipt.
    
    * Fixing Kata1
    
    * Kata1 Fixed
    
    * Kata1 fixed, Correct wordLessFive for wordMoreFive
    
    * Kata1 fixing
    
    * Kata1 fixed again
    
    * Kata1 Fixing again
    
    * Update Kata1Test.java
    
    * Kata1 fixed using StringJoiner
    
    * Kata1 fixed by StringJoiner

---
+++ src/main/java/org/fundacionjala/coding/ariel/Kata1.java
@@ -0,0 +1,25 @@
+package org.fundacionjala.coding.ariel;
+
+import java.util.StringJoiner;
+
+/**
+ * Kata1.
+ * Ariel Gonzales V.
+ */
+public class Kata1 {
+    private static final int CONDITION = 5;
+
+    /**
+     * @param text Any String Text.
+     * @return String.
+     */
+    public String rotReverseWord(String text) {
+        StringJoiner word = new StringJoiner(" ");
+        String[] texts = text.split(" ");
+        for (String t : texts) {
+
+            word = t.length() >= CONDITION ? word.add(new StringBuilder(t).reverse()) : word.add(t);
+        }
+        return word.toString();
+    }
+}
