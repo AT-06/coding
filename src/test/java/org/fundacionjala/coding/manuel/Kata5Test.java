062281aa7e67f6643abf6ef44b8844f118e5b3cd
manu863018 <Manuel.Valdez@fundacion-jala.org>
2018-03-06 20:18:13 +0000

    Kata5 correct mistakes

--- src/test/java/org/fundacionjala/coding/manuel/Kata5Test.java
+++ src/test/java/org/fundacionjala/coding/manuel/Kata5Test.java
@@ -1,5 +1,6 @@
 package org.fundacionjala.coding.manuel;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -8,13 +9,25 @@ import static org.junit.Assert.assertEquals;
  */
 public class Kata5Test {
 
+    private Kata5 dnaStrand;
+
+    /**
+     * Method to run before tests.
+     */
+    @Before public void inicialize() {
+        dnaStrand = new Kata5();
+    }
     /**
      * First test method makeComplement.
      */
     @Test
     public void test1MakeComplementKata5() {
-        Kata5 dnaStrand = new Kata5();
         assertEquals("TTTT", dnaStrand.makeComplement("AAAA"));
+        assertEquals("TAACG", dnaStrand.makeComplement("ATTGC"));
+        assertEquals("CATA", dnaStrand.makeComplement("GTAT"));
+        assertEquals("TTCC", dnaStrand.makeComplement("AAGG"));
+        assertEquals("GCGC", dnaStrand.makeComplement("CGCG"));
+        assertEquals("TAACG", dnaStrand.makeComplement("ATTGC"));
     }
 
     /**
@@ -22,8 +35,9 @@ public class Kata5Test {
      */
     @Test
     public void test2MakeComplementKata5() {
-        Kata5 dnaStrand = new Kata5();
-        assertEquals("TAACG", dnaStrand.makeComplement("ATTGC"));
+        String expected = "CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT";
+        String param = "GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA";
+        assertEquals(expected, dnaStrand.makeComplement(param));
     }
 
     /**
@@ -31,54 +45,6 @@ public class Kata5Test {
      */
     @Test
     public void test3MakeComplementKata5() {
-        Kata5 dnaStrand = new Kata5();
-        assertEquals("CATA", dnaStrand.makeComplement("GTAT"));
-    }
-
-    /**
-     * Fourth test method makeComplement.
-     */
-    @Test
-    public void test4MakeComplementKata5() {
-        Kata5 dnaStrand = new Kata5();
-        assertEquals("TTCC", dnaStrand.makeComplement("AAGG"));
-    }
-
-    /**
-     * Fifth test method makeComplement.
-     */
-    @Test
-    public void test5MakeComplementKata5() {
-        Kata5 dnaStrand = new Kata5();
-        assertEquals("GCGC", dnaStrand.makeComplement("CGCG"));
-    }
-
-    /**
-     * Sixth test method makeComplement.
-     */
-    @Test
-    public void test6MakeComplementKata5() {
-        Kata5 dnaStrand = new Kata5();
-        assertEquals("TAACG", dnaStrand.makeComplement("ATTGC"));
-    }
-
-    /**
-     * Seventh test method makeComplement.
-     */
-    @Test
-    public void test7MakeComplementKata5() {
-        Kata5 dnaStrand = new Kata5();
-        String expected = "CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT";
-        String param = "GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA";
-        assertEquals(expected, dnaStrand.makeComplement(param));
-    }
-
-    /**
-     * Eight test method makeComplement.
-     */
-    @Test
-    public void test8MakeComplementKata5() {
-        Kata5 dnaStrand = new Kata5();
         assertEquals("TAACGX", dnaStrand.makeComplement("ATTGCX"));
     }
 }
