c806dfde93b594523cb72270f40284014e95ba64
pipo411 <Ariel.Gonzales@fundacion-jala.org>
2018-03-07 18:23:25 -0400

    Kata7 upload again solved by IF

--- src/test/java/org/fundacionjala/coding/ariel/Kata7Test.java
+++ src/test/java/org/fundacionjala/coding/ariel/Kata7Test.java
@@ -1,5 +1,26 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.ariel;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Ariel Gonzales V..
+ * Test to check the FizzBuzzNumber method.
+ */
 public class Kata7Test {
+    /**
+     * Conditions.
+     */
+    @Test
+    public void fizzBuzzNumber() {
+        Kata7 katita = new Kata7();
+        assertEquals("8", katita.fizzBuzz("8"));
+        assertEquals("FizzBuzz", katita.fizzBuzz("15"));
+        assertEquals("Buzz", katita.fizzBuzz("5"));
+        assertEquals("Fizz", katita.fizzBuzz("3"));
+        assertEquals("28", katita.fizzBuzz("28"));
+
+    }
 
 }
