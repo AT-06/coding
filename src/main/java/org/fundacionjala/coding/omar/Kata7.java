39506f8a6a8e25e948f12a3e2e1736a50cad6b27
Omar Huanca Sanchez <omar.huanca@fundacion-jala.org>
2018-03-07 17:52:03 -0400

    Kata 7 is completed
    Kata 7 is completed

---
+++ src/main/java/org/fundacionjala/coding/omar/Kata7.java
@@ -0,0 +1,29 @@
+package org.fundacionjala.coding.omar;
+
+/**
+ * This class contains a method that prints the numbers greater to 0.
+ * But for multiples of three print “Fizz” instead of the number
+ * and for the multiples of five print “Buzz”.
+ * For numbers which are multiples of both three and five print “FizzBuzz “.
+ */
+public class Kata7 {
+
+
+    private static final int NUMBER_15 = 15;
+    private static final int ZERO = 0;
+    private static final int NUMBER_3 = 3;
+    private static final int NUMBER_5 = 5;
+
+    /**
+     * @param number this is words for Spin Words
+     * @return result
+     */
+    public String isFizzBuzzNumer(int number) {
+
+        return number % NUMBER_15 == ZERO
+                ? "FizzBuzz" : number % NUMBER_3 == ZERO
+                ? "Fizz" : number % NUMBER_5 == ZERO
+                ? "Buzz" : String.valueOf(number);
+    }
+
+}
