e9e4a687a07a49c5acb188f032a077e41637610a
Daniel Caballero <daniel.caballero@fundacion-jala.org>
2018-03-08 16:19:15 -0400

    Kata7 changed

--- src/main/java/org/fundacionjala/coding/daniel/Kata7.java
+++ src/main/java/org/fundacionjala/coding/daniel/Kata7.java
@@ -2,12 +2,14 @@ package org.fundacionjala.coding.daniel;
 
 public class Kata7 {
     /**
-     * @param number
-     * @return
+     * @param number a integer.
+     * @return a String whether the number is 'Fizz', 'Buzz' or 'FizzBuzz'.
      */
     public static String fizzBuzz(int number) {
         return number % 3 == 0 ? number % 5 == 0
             ? "FizzBuzz" : "Buzz" : number % 5 == 0
             ? "Fizz" : String.valueOf(number);
     }
+
+    public 
 }
