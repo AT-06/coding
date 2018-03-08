0830d54b6008b4fceb0f6379fa438b5748b33f1f
JimmyRomero <jimmy.romero@fundacion-jala.org>
2018-03-08 16:20:11 -0400

    Kata7 developed

--- src/main/java/org/fundacionjala/coding/jimmy/Kata7.java
+++ src/main/java/org/fundacionjala/coding/jimmy/Kata7.java
@@ -1,4 +1,34 @@
 package org.fundacionjala.coding.jimmy;
 
+/**
+ * Class for developing giveFizzBuzzNumbers method.
+ */
 public class Kata7 {
+
+    public static final int MULTIPLEOFTHREE = 3;
+    public static final int MULTIPLEOFFIVE = 5;
+    public static final int MULTIPLEOFTHREEANDFIVE = 15;
+
+    /**
+     * This method is for knowing whether a number is Fizz, Buzz or FizzBuzz.
+     *
+     * @param numberFizzBuzz is the number to calculate.
+     * @return whether the number is Fizz, Buzz or FizzBuzz.
+     */
+    public String giveFizzBuzzNumbers(int numberFizzBuzz) {
+
+        String fizzBuzzResult = String.valueOf(numberFizzBuzz);
+        String value = String.valueOf(numberFizzBuzz);
+
+        if (numberFizzBuzz % MULTIPLEOFTHREE == 0 || value.contains("3")) {
+            fizzBuzzResult = "Fizz";
+        }
+        if (numberFizzBuzz % MULTIPLEOFFIVE == 0 || value.contains("5")) {
+            fizzBuzzResult = "Buzz";
+        }
+        if (numberFizzBuzz % MULTIPLEOFTHREEANDFIVE == 0) {
+            fizzBuzzResult = "FizzBuzz";
+        }
+        return fizzBuzzResult;
+    }
 }
--- src/test/java/org/fundacionjala/coding/jimmy/Kata7Test.java
+++ src/test/java/org/fundacionjala/coding/jimmy/Kata7Test.java
@@ -1,5 +1,130 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.jimmy;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This class is for testing the giveFizzBuzzNumbers method.
+ */
 public class Kata7Test {
 
+    private Kata7 kata7;
+
+    /**
+     * This method initializes kata7.
+     */
+    @Before
+    public void initialize() {
+        kata7 = new Kata7();
+    }
+
+    /**
+     * Test for testing the method's expected and actual results.
+     */
+    @Test
+    public void testFizzBuzz1() {
+        final int numberFizzBuzz = 3;
+
+        assertEquals("Fizz", kata7.giveFizzBuzzNumbers(numberFizzBuzz));
+    }
+
+    /**
+     * Test for testing the method's expected and actual results.
+     */
+    @Test
+    public void testFizzBuzz2() {
+        final int numberFizzBuzz = 5;
+
+        assertEquals("Buzz", kata7.giveFizzBuzzNumbers(numberFizzBuzz));
+    }
+
+    /**
+     * Test for testing the method's expected and actual results.
+     */
+    @Test
+    public void testFizzBuzz3() {
+        final int numberFizzBuzz = 1;
+
+        final String expected = "1";
+        assertEquals(expected, kata7.giveFizzBuzzNumbers(numberFizzBuzz));
+    }
+
+    /**
+     * Test for testing the method's expected and actual results.
+     */
+    @Test
+    public void testFizzBuzz4() {
+        final int numberFizzBuzz = 7;
+
+        final String expected = "7";
+        assertEquals(expected, kata7.giveFizzBuzzNumbers(numberFizzBuzz));
+    }
+
+    /**
+     * Test for testing the method's expected and actual results.
+     */
+    @Test
+    public void testFizzBuzz5() {
+        final  int numberFizzBuzz = 12;
+
+        final String expected = "Fizz";
+        assertEquals(expected, kata7.giveFizzBuzzNumbers(numberFizzBuzz));
+    }
+
+    /**
+     * Test for testing the method's expected and actual results.
+     */
+    @Test
+    public void testFizzBuzz6() {
+        final int numberFizzBuzz = 13;
+
+        final String expected = "Fizz";
+        assertEquals(expected, kata7.giveFizzBuzzNumbers(numberFizzBuzz));
+    }
+
+    /**
+     * Test for testing the method's expected and actual results.
+     */
+    @Test
+    public void testFizzBuzz7() {
+        final  int numberFizzBuzz = 15;
+
+        final String expected = "FizzBuzz";
+        assertEquals(expected, kata7.giveFizzBuzzNumbers(numberFizzBuzz));
+    }
+
+    /**
+     * Test for testing the method's expected and actual results.
+     */
+    @Test
+    public void testFizzBuzz8() {
+        final int numberFizzBuzz = 20;
+
+        final String expected = "Buzz";
+        assertEquals(expected, kata7.giveFizzBuzzNumbers(numberFizzBuzz));
+    }
+
+    /**
+     * Test for testing the method's expected and actual results.
+     */
+    @Test
+    public void testFizzBuzz9() {
+        final int numberFizzBuzz = 30;
+
+        final String expected = "FizzBuzz";
+        assertEquals(expected, kata7.giveFizzBuzzNumbers(numberFizzBuzz));
+    }
+
+    /**
+     * Test for testing the method's expected and actual results.
+     */
+    @Test
+    public void testFizzBuzz10() {
+        final int numberFizzBuzz = 54;
+
+        final String expected = "Buzz";
+        assertEquals(expected, kata7.giveFizzBuzzNumbers(numberFizzBuzz));
+    }
 }
