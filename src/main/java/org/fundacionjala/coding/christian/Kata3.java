cd5ae7c9bbee66ec8022a5f92caad14f363e6df4
Christian Galarza <Christian.Galarza@fundacion-jala.org>
2018-03-07 21:59:44 -0400

    Kata3 corrected

---
+++ src/main/java/org/fundacionjala/coding/christian/Kata3.java
@@ -0,0 +1,25 @@
+package org.fundacionjala.coding.christian;
+
+import java.util.Arrays;
+
+/**
+ * Develop by Christian.
+ */
+public class Kata3 {
+    private static final int LIM = 10;
+
+    /**
+     * @param num this is for Persistence
+     * @return tag for 'num'
+     */
+    public  int persistence(int num) {
+        int cont = 0;
+        while (num / LIM > 0) {
+            int[] digits = Integer.toString(num).chars().map(f -> f -= '0').toArray();
+            num = 1;
+            num *= Arrays.stream(digits).reduce(1, (a, b) -> a * b);
+            cont++;
+        }
+        return  cont;
+    }
+}
