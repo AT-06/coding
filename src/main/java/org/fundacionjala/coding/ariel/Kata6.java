8414c8d3f6108b8b483cd7ae389e51cd0eca90e4
pipo411 <Ariel.Gonzales@fundacion-jala.org>
2018-03-11 19:36:54 -0400

    Kata6 solved

---
+++ src/main/java/org/fundacionjala/coding/ariel/Kata6.java
@@ -0,0 +1,39 @@
+package org.fundacionjala.coding.ariel;
+
+import java.util.TreeMap;
+
+/**
+ * Kata6.
+ * by Ariel Gonzales Vargas.
+ */
+public class Kata6 {
+
+    /**
+     * @param numbers An integer array.
+     * @return An changed sortTwisted37 array..
+     */
+    public Integer[] sortTwisted37(Integer[] numbers) {
+        TreeMap<Integer, Integer> series = new TreeMap<Integer, Integer>();
+        Integer[] result = new Integer[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            String condition = String.valueOf(numbers[i]);
+            if (condition.contains("3") || condition.contains("7")) {
+                int key = Integer.parseInt(condition.replace('3', '0').replace('7', '3').replace('0', '7'));
+                series.put(key, numbers[i]);
+            } else {
+                series.put(numbers[i], numbers[i]);
+            }
+        }
+
+        Integer[] claves = series.keySet().toArray(new Integer[0]);
+        int i = 0;
+        for (Integer key : claves) {
+            result[i] = series.get(key);
+            i++;
+        }
+
+        return result;
+    }
+}
+
---
+++ src/test/java/org/fundacionjala/coding/ariel/Kata6Test.java
@@ -0,0 +1,44 @@
+package org.fundacionjala.coding.ariel;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unittests.
+ */
+public class Kata6Test {
+
+    /**
+     * Condition1_BasicTests..
+     */
+    @Test
+    public void basicTests() {
+        Kata6 katita = new Kata6();
+        final Integer[] result1 = {1, 2, 7, 4, 5, 6, 3, 8, 9};
+        final Integer[] input1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertEquals(Arrays.toString(result1), Arrays.toString(katita.sortTwisted37(input1)));
+        final Integer[] result2 = {12, 14, 13};
+        final Integer[] input2 = {12, 13, 14};
+        assertEquals(Arrays.toString(result2), Arrays.toString(katita.sortTwisted37(input2)));
+        final Integer[] result3 = {2, 7, 4, 3, 9};
+        final Integer[] input3 = {9, 2, 4, 7, 3};
+        assertEquals(Arrays.toString(result3), Arrays.toString(katita.sortTwisted37(input3)));
+    }
+
+    /**
+     * Condition2_UnChangedArrayTest.
+     */
+    @Test
+    public void unChangedArrayTest() {
+        Kata6 katita = new Kata6();
+        final Integer[] array = new Integer[]{9, 2, 4, 7, 3};
+        final Integer[] arrayCpy = new Integer[]{9, 2, 4, 7, 3};
+        final Integer[] result = {2, 7, 4, 3, 9};
+        assertEquals(Arrays.toString(result), Arrays.toString(katita.sortTwisted37(array)));
+        assertEquals("You should not modify the input array!", Arrays.toString(arrayCpy), Arrays.toString(array));
+    }
+
+}
