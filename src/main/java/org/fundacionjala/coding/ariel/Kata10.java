f9a50f912845c785cb06792c861f30be9989f78d
pipo411 <Ariel.Gonzales@fundacion-jala.org>
2018-03-12 17:17:14 -0400

    Kata10 solved by Sort

--- src/main/java/org/fundacionjala/coding/ariel/Kata10.java
+++ src/main/java/org/fundacionjala/coding/ariel/Kata10.java
@@ -1,4 +1,28 @@
 package org.fundacionjala.coding.ariel;
 
+import java.util.Arrays;
+import java.util.StringJoiner;
+
+/**
+ * Kata 10.
+ * By Ariel Gonzales Vargas..
+ */
 public class Kata10 {
+    /**
+     * @param serie An string of numbers.
+     * @return An string that contains the High and low numbers.
+     */
+    public String highAndLow(String serie) {
+        StringJoiner resp = new StringJoiner(" ");
+        String[] numbers = serie.split(" ");
+        int[] order = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            order[i] = Integer.parseInt(numbers[i]);
+        }
+        Arrays.sort(order);
+        resp.add(String.valueOf(order[order.length - 1])).add(String.valueOf(order[0]));
+        return resp.toString();
+    }
+
 }
--- src/test/java/org/fundacionjala/coding/ariel/Kata10Test.java
+++ src/test/java/org/fundacionjala/coding/ariel/Kata10Test.java
@@ -1,5 +1,85 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.ariel;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unittests.
+ */
 public class Kata10Test {
+    /**
+     * Chechk the someTest..
+     */
+    @Test
+    public void someTest() {
+        Kata10 katita = new Kata10();
+        assertEquals("542 -214", katita.highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
+    }
+
+    /**
+     * Check the plusMinusTest.
+     */
+    @Test
+    public void plusMinusTest() {
+        Kata10 katita = new Kata10();
+        assertEquals("1 -1", katita.highAndLow("1 -1"));
+    }
+
+    /**
+     * Check the plusPlusTest.
+     */
+    @Test
+    public void plusPlusTest() {
+        Kata10 katita = new Kata10();
+        assertEquals("1 1", katita.highAndLow("1 1"));
+    }
+
+    /**
+     * Check the minusMinutTest.
+     */
+    @Test
+    public void minusMinusTest() {
+        Kata10 katita = new Kata10();
+        assertEquals("-1 -1", katita.highAndLow("-1 -1"));
+    }
+
+    /**
+     * Check the plusMinusZeroTest.
+     */
+    @Test
+    public void plusMinusZeroTest() {
+        Kata10 katita = new Kata10();
+        assertEquals("1 -1", katita.highAndLow("1 -1 0"));
+    }
+
+
+    /**
+     * Check the plusPlusZeroTest.
+     */
+    @Test
+    public void plusPlusZeroTest() {
+        Kata10 katita = new Kata10();
+        assertEquals("1 0", katita.highAndLow("1 1 0"));
+    }
+
+    /**
+     * Check the minusMinusZeroTest.
+     */
+    @Test
+    public void minusMinusZeroTest() {
+        Kata10 katita = new Kata10();
+        assertEquals("0 -1", katita.highAndLow("-1 -1 0"));
+    }
+
+
+    /**
+     * Check the singleTest.
+     */
+    @Test
+    public void singleTest() {
+        Kata10 katita = new Kata10();
+        assertEquals("42 42", katita.highAndLow("42"));
+    }
 
 }
