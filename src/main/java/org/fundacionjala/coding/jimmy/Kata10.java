c56e87f6c8235cbdd625502fa0b72581148253ae
JimmyRomero <jimmy.romero@fundacion-jala.org>
2018-03-12 18:27:59 -0400

    Replacing with string format

--- src/main/java/org/fundacionjala/coding/jimmy/Kata10.java
+++ src/main/java/org/fundacionjala/coding/jimmy/Kata10.java
@@ -3,7 +3,6 @@ package org.fundacionjala.coding.jimmy;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.StringJoiner;
 
 /**
  * This class is for implementing the highAndLow method.
@@ -26,11 +25,8 @@ public class Kata10 {
         for (String i : arrayNumber) {
             numberArray.add(Integer.parseInt(i));
         }
-        StringJoiner stringJoiner = new StringJoiner(BLANK_SPACE);
 
-        stringJoiner.add(Collections.max(numberArray).toString());
-        stringJoiner.add(Collections.min(numberArray).toString());
-
-        return String.valueOf(stringJoiner);
+        return String.format("%s %s", Collections.max(numberArray).toString(),
+                Collections.min(numberArray).toString());
     }
 }
