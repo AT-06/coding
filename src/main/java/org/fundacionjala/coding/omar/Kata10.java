e3b2105af41e056bf9ba5b72f242d2887228e26f
Omar Huanca Sanchez <omar.huanca@fundacion-jala.org>
2018-03-12 17:04:52 -0400

    completed
    completed

---
+++ src/main/java/org/fundacionjala/coding/omar/Kata10.java
@@ -0,0 +1,27 @@
+package org.fundacionjala.coding.omar;
+
+import java.util.stream.Stream;
+/**
+ * Powered by Omar Limbert Huanca Sanchez - AT06.
+ * In this little assignment you are given a string
+ * of space separated numbers, and have to return
+ * the highest and lowest number.
+ */
+public class Kata10 {
+
+    private static final String BLACK_SPACE = " ";
+
+    /**
+     * @param numbers This is a number for find high and low.
+     * @return result
+     */
+    public String highAndLow(String numbers) {
+        Integer[] sorted = Stream.of(numbers.split(BLACK_SPACE))
+                .mapToInt(Integer::valueOf)
+                .sorted()
+                .boxed()
+                .toArray(Integer[]::new);
+
+        return sorted[sorted.length - 1]  + BLACK_SPACE + sorted[0];
+    }
+}
