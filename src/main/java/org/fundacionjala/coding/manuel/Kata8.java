cced318d37664338bc7c8c8de05a4dcda826bb16
manu863018 <manuel.valdez@fundacion-jala.org>
2018-03-11 23:50:32 -0400

    Kata8 first solution.

--- src/main/java/org/fundacionjala/coding/manuel/Kata8.java
+++ src/main/java/org/fundacionjala/coding/manuel/Kata8.java
@@ -1,4 +1,87 @@
 package org.fundacionjala.coding.manuel;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+/**
+ * Created by Juan Manuel on 3/11/2018.
+ */
 public class Kata8 {
+    private Map<String, String> mapedValues = new HashMap<>();
+    private static final int SUFIX = 3;
+    private static final int MOD_NUMBER = 11;
+    /**
+     * Constructor.
+     */
+    public Kata8() {
+        mapedValues.put(" _ | ||_|", "0");
+        mapedValues.put("     |  |", "1");
+        mapedValues.put(" _  _||_ ", "2");
+        mapedValues.put(" _  _| _|", "3");
+        mapedValues.put("   |_|  |", "4");
+        mapedValues.put(" _ |_  _|", "5");
+        mapedValues.put(" _ |_ |_|", "6");
+        mapedValues.put(" _   |  |", "7");
+        mapedValues.put(" _ |_||_|", "8");
+        mapedValues.put(" _ |_| _|", "9");
+    }
+
+    /**
+     * Method bank to convert number.
+     * @param firstRow first row of pipes and underscore.
+     * @param secondRow second row of pipes and underscore.
+     * @param thirdRow third row of pipes and underscore.
+     * @return numberValue converted.
+     */
+    public String bank(String firstRow, String secondRow, String thirdRow) {
+        StringJoiner numberValue = new StringJoiner("");
+        String concatedNumber;
+        while (!firstRow.isEmpty()) {
+            concatedNumber = concatNumber(firstRow, secondRow, thirdRow);
+            numberValue.add(mapedValues.containsKey(concatedNumber) ? mapedValues.get(concatedNumber) : "?");
+            firstRow = firstRow.substring(SUFIX);
+            secondRow = secondRow.substring(SUFIX);
+            thirdRow = thirdRow.substring(SUFIX);
+        }
+        if (numberValue.toString().contains("?")) {
+            numberValue.add(" ILL");
+            return numberValue.toString();
+        }
+        boolean validAccount = calculateCheckSum(numberValue.toString());
+        if (!validAccount) {
+            numberValue.add(" ERR");
+            return numberValue.toString();
+        }
+        return numberValue.toString();
+    }
+
+    /**
+     * Method concatNumber to concat pipes and underscores.
+     * @param firstRow first row of pipes and underscore.
+     * @param secondRow second row of pipes and underscore.
+     * @param thirdRow third row of pipes and underscore.
+     * @return pipes and lines concated for first number.
+     */
+    public String concatNumber(String firstRow, String secondRow, String thirdRow) {
+        StringJoiner join = new StringJoiner("");
+        join.add(firstRow.substring(0, SUFIX));
+        join.add(secondRow.substring(0, SUFIX));
+        join.add(thirdRow.substring(0, SUFIX));
+        return join.toString();
+    }
+
+    /**
+     * Method calculateCheckSum to check if it is a valid account.
+     * @param number number converted.
+     * @return boolean value if the account is real.
+     */
+    public boolean calculateCheckSum(String number) {
+        int sum = 0;
+        for (int i = number.length(); i > 0; i--) {
+            sum += Integer.parseInt(number.substring(0, 1)) * i;
+            number = number.substring(1);
+        }
+        return sum % MOD_NUMBER == 0;
+    }
 }
--- src/test/java/org/fundacionjala/coding/manuel/Kata8Test.java
+++ src/test/java/org/fundacionjala/coding/manuel/Kata8Test.java
@@ -1,5 +1,155 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.manuel;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+/**
+ * Created by Juan Manuel on 3/11/2018.
+ */
 public class Kata8Test {
+    private Kata8 kata8;
+    private static final String COMMON_LINE1 = " _  _  _  _  _  _  _  _  _ ";
+    private static final String COMMON_LINE2 = "|_||_||_||_||_||_||_||_||_|";
+    private static final String COMMON_LINE3 = "  |  |  |  |  |  |  |  |  |";
+    private static final String COMMON_LINE4 = " _| _| _| _| _| _| _| _| _|";
+    /**
+     * Method to initialize.
+     */
+    @Before
+    public void initialize() {
+        kata8 = new Kata8();
+    }
 
+    /**
+     * First test.
+     */
+    @Test
+    public void test1BankKata8() {
+        //" _  _  _  _  _  _  _  _  _ ";
+        String secondRow = "| || || || || || || || || |";
+        //"|_||_||_||_||_||_||_||_||_|";
+        assertEquals("000000000", kata8.bank(COMMON_LINE1, secondRow, COMMON_LINE2));
+    }
+
+    /**
+     * Second test.
+     */
+    @Test
+    public void test2BankKata8() {
+        String firstRow =  "                           ";
+        //"  |  |  |  |  |  |  |  |  |";
+        //"  |  |  |  |  |  |  |  |  |";
+        assertEquals("111111111 ERR", kata8.bank(firstRow, COMMON_LINE3, COMMON_LINE3));
+    }
+
+    /**
+     * Third test.
+     */
+    @Test
+    public void test3BankKata8() {
+        //" _  _  _  _  _  _  _  _  _ ";
+        //" _| _| _| _| _| _| _| _| _|";
+        String thirdRow =  "|_ |_ |_ |_ |_ |_ |_ |_ |_ ";
+        assertEquals("222222222 ERR", kata8.bank(COMMON_LINE1, COMMON_LINE4, thirdRow));
+    }
+
+    /**
+     * Fourth test.
+     */
+    @Test
+    public void test4BankKata8() {
+        //" _  _  _  _  _  _  _  _  _ ";
+        //" _| _| _| _| _| _| _| _| _|";
+        //" _| _| _| _| _| _| _| _| _|";
+        assertEquals("333333333 ERR", kata8.bank(COMMON_LINE1, COMMON_LINE4, COMMON_LINE4));
+    }
+
+    /**
+     * Fifth test.
+     */
+    @Test
+    public void test5BankKata8() {
+        String firstRow =  "                           ";
+        //"|_||_||_||_||_||_||_||_||_|";
+        //"  |  |  |  |  |  |  |  |  |";
+        assertEquals("444444444 ERR", kata8.bank(firstRow, COMMON_LINE2, COMMON_LINE3));
+    }
+
+    /**
+     * Sixth test.
+     */
+    @Test
+    public void test6BankKata8() {
+        //" _  _  _  _  _  _  _  _  _ ";
+        String secondRow = "|_ |_ |_ |_ |_ |_ |_ |_ |_ ";
+        //" _| _| _| _| _| _| _| _| _|";
+        assertEquals("555555555 ERR", kata8.bank(COMMON_LINE1, secondRow, COMMON_LINE4));
+    }
+
+    /**
+     * Seventh test.
+     */
+    @Test
+    public void test7BankKata8() {
+        //" _  _  _  _  _  _  _  _  _ ";
+        String secondRow = "|_ |_ |_ |_ |_ |_ |_ |_ |_ ";
+        //"|_||_||_||_||_||_||_||_||_|";
+        assertEquals("666666666 ERR", kata8.bank(COMMON_LINE1, secondRow, COMMON_LINE2));
+    }
+
+    /**
+     * Eighth test.
+     */
+    @Test
+    public void test8BankKata8() {
+        //" _  _  _  _  _  _  _  _  _ ";
+        //"  |  |  |  |  |  |  |  |  |";
+        //"  |  |  |  |  |  |  |  |  |";
+        assertEquals("777777777 ERR", kata8.bank(COMMON_LINE1, COMMON_LINE3, COMMON_LINE3));
+    }
+
+    /**
+     * Nineth test.
+     */
+    @Test
+    public void test9BankKata8() {
+        //" _  _  _  _  _  _  _  _  _ ";
+        //"|_||_||_||_||_||_||_||_||_|";
+        //"|_||_||_||_||_||_||_||_||_|";
+        assertEquals("888888888 ERR", kata8.bank(COMMON_LINE1, COMMON_LINE2, COMMON_LINE2));
+    }
+
+    /**
+     * Tenth test.
+     */
+    @Test
+    public void test10BankKata8() {
+        //" _  _  _  _  _  _  _  _  _ ";
+        //"|_||_||_||_||_||_||_||_||_|";
+        //" _| _| _| _| _| _| _| _| _|";
+        assertEquals("999999999 ERR", kata8.bank(COMMON_LINE1, COMMON_LINE2, COMMON_LINE4));
+    }
+
+    /**
+     * Eleventh test.
+     */
+    @Test
+    public void test11BankKata8() {
+        String firstRow =  "    _  _     _  _  _  _  _ ";
+        String secondRow = "  | _| _||_||_ |_   ||_||_|";
+        String thirdRow =  "  ||_  _|  | _||_|  ||_| _|";
+        assertEquals("123456789", kata8.bank(firstRow, secondRow, thirdRow));
+    }
+
+    /**
+     * Twelfth test.
+     */
+    @Test
+    public void test12BankKata8() {
+        String firstRow =  "    _  _     _  _  _  _  _ ";
+        String secondRow = "  | _| _||_||_ |_   ||_||  ";
+        String thirdRow =  "  ||_  _|  | _||_|  ||_| _|";
+        assertEquals("12345678? ILL", kata8.bank(firstRow, secondRow, thirdRow));
+    }
 }
