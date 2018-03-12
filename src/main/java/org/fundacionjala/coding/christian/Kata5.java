5b9711c040fc257624eb17901c6e4b05a990fd29
Chritian92 <35768795+Chritian92@users.noreply.github.com>
2018-03-12 19:17:40 -0400

    Kata5 Completed (#58)
    * Kata5 corrected
    
    * Kata5 fixed
    
    * Update Kata5.java

---
+++ src/main/java/org/fundacionjala/coding/christian/Kata5.java
@@ -0,0 +1,30 @@
+package org.fundacionjala.coding.christian;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Develop by Christian.
+ */
+public class Kata5 {
+
+    /**
+     * @param dna hi is for makeComplement
+     * @return tag for 'dna'
+     */
+    public String makeComplement(String dna) {
+
+        StringBuilder builder = new StringBuilder();
+        Map<String, String> dnaMap = new HashMap<>();
+        dnaMap.put("A", "T");
+        dnaMap.put("T", "A");
+        dnaMap.put("G", "C");
+        dnaMap.put("C", "G");
+        String[] letters = dna.split("");
+        for (String  letter : letters) {
+            builder.append(dnaMap.get(letter));
+        }
+         return builder.toString();
+     }
+
+}
