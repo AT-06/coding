280e5e228cd3a53db78d4e47a2f3fa67eda70a41
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-03-12 18:57:29 -0400

    kata 7 changes

--- src/main/java/org/fundacionjala/coding/maria/Kata7.java
+++ src/main/java/org/fundacionjala/coding/maria/Kata7.java
@@ -7,20 +7,20 @@ package org.fundacionjala.coding.maria;
 public class Kata7 {
     private static final int THREE = 3;
     private static final int FIVE = 5;
-    private static final String BLANK = "";
+
     /**
      * @param num int
      * @return string fizz/buzz/num
      */
     public String fizzBuzz(int num) {
         String numString =  Integer.toString(num);
-        String result = BLANK;
+        String result = "";
         if (num % THREE == 0 || numString.contains("3")) {
             result = result.concat("Fizz");
         }
         if (num % FIVE == 0 || numString.contains("5")) {
             result = result.concat("Buzz");
         }
-        return result.equals(BLANK) ? numString : result;
+        return result.isEmpty() ? numString : result;
     }
 }
