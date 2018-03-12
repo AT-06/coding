c5e1b706ad795c3ff3f0691fa4543a758dbf60f2
manu863018 <Manuel.Valdez@fundacion-jala.org>
2018-03-12 21:00:25 +0000

    Kata8 correction first solution.

--- src/main/java/org/fundacionjala/coding/manuel/Kata8.java
+++ src/main/java/org/fundacionjala/coding/manuel/Kata8.java
@@ -8,13 +8,15 @@ import java.util.StringJoiner;
  * Created by Juan Manuel on 3/11/2018.
  */
 public class Kata8 {
-    private Map<String, String> mapedValues = new HashMap<>();
+
     private static final int SUFIX = 3;
     private static final int MOD_NUMBER = 11;
+    private static Map<String, String> mapedValues = new HashMap<>();
     /**
-     * Constructor.
+     * static initializer.
      */
-    public Kata8() {
+    static {
+
         mapedValues.put(" _ | ||_|", "0");
         mapedValues.put("     |  |", "1");
         mapedValues.put(" _  _||_ ", "2");
@@ -44,18 +46,23 @@ public class Kata8 {
             secondRow = secondRow.substring(SUFIX);
             thirdRow = thirdRow.substring(SUFIX);
         }
-        if (numberValue.toString().contains("?")) {
-            numberValue.add(" ILL");
-            return numberValue.toString();
-        }
-        boolean validAccount = calculateCheckSum(numberValue.toString());
-        if (!validAccount) {
-            numberValue.add(" ERR");
-            return numberValue.toString();
-        }
-        return numberValue.toString();
+        return addSufixes(numberValue.toString());
     }
 
+    /**
+     * Method defineSufixes to concat sufixes or not to account.
+     * @param numberValue value with the account and ?.
+     * @return account with ILL or ERR or valid account.
+     */
+    public String addSufixes(String numberValue) {
+        if (numberValue.contains("?")) {
+            return numberValue.concat(" ILL");
+        }
+        if (!calculateCheckSum(numberValue)) {
+            return numberValue.concat(" ERR");
+        }
+        return numberValue;
+    }
     /**
      * Method concatNumber to concat pipes and underscores.
      * @param firstRow first row of pipes and underscore.
