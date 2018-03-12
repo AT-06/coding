c1f200649d3f50cd6a4d42e41d1e0827201aaf9f
Christian Galarza <Christian.Galarza@fundacion-jala.org>
2018-03-12 19:03:32 -0400

    Kata2 fixed

--- src/main/java/org/fundacionjala/coding/christian/Kata2.java
+++ src/main/java/org/fundacionjala/coding/christian/Kata2.java
@@ -2,8 +2,9 @@ package org.fundacionjala.coding.christian;
 /**
  * Development by Christian
  */
+
 import java.util.Arrays;
-import  java.util.Collections;
+import java.util.Collections;
 import java.util.StringJoiner;
 
 /**
@@ -15,20 +16,19 @@ public class Kata2 {
     /**
      * @param words this is for Word
      * @return tag for 'words'
-     * */
+     */
     public String word(String words) {
         String[] wordsList = words.split(" ");
-        StringJoiner joiner = new StringJoiner("");
-        for (int i = 0; i < wordsList.length; i++) {
-            String[] changedPosition = wordsList[i].split("");
-            if (changedPosition.length > LIST_LENGTH) {
+        StringJoiner joiner = new StringJoiner(" ");
+        for (String aWordsList : wordsList) {
+
+            if (aWordsList.length() > LIST_LENGTH) {
+                String[] changedPosition = aWordsList.split("");
                 Arrays.sort(changedPosition, 1, changedPosition.length - 1, Collections.reverseOrder());
+                joiner.add(String.join("", changedPosition));
             }
-            for (String letter : changedPosition) {
-                joiner.add(letter);
-            }
-            if (i < wordsList.length - 1) {
-                joiner.add(" ");
+            else {
+                joiner.add(aWordsList);
             }
         }
         return joiner.toString();
