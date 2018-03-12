eb0ac4fcc6006dc85c88b7a0496e911c73fcda9e
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-03-12 18:54:57 -0400

    kata 8 commit 2

--- src/main/java/org/fundacionjala/coding/maria/Kata8.java
+++ src/main/java/org/fundacionjala/coding/maria/Kata8.java
@@ -1,4 +1,64 @@
 package org.fundacionjala.coding.maria;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+/**
+ * Kata 8 bank number.
+ */
 public class Kata8 {
+    private static final int THREE = 3;
+    private static final int ELEVEN = 11;
+    private static final int NINE = 9;
+    private static Map<String, String> numberEqui = new HashMap<>();
+    static {
+        numberEqui.put(" _ | ||_|", "0");
+        numberEqui.put("     |  |", "1");
+        numberEqui.put(" _  _||_ ", "2");
+        numberEqui.put(" _  _| _|", "3");
+        numberEqui.put("   |_|  |", "4");
+        numberEqui.put(" _ |_  _|", "5");
+        numberEqui.put(" _ |_ |_|", "6");
+        numberEqui.put(" _   |  |", "7");
+        numberEqui.put(" _ |_||_|", "8");
+        numberEqui.put(" _ |_| _|", "9");
+    }
+
+    /**
+     * @param filaUno fila uno string
+     * @param filaDos fila dos string
+     * @param filaTres fila tres string
+     * @return numero
+     */
+    public String bankOCR(String filaUno, String filaDos, String filaTres) {
+        int modEleven = 0;
+        StringJoiner onlyNumber = new StringJoiner("");
+        for (int i = NINE; i >= 1; i--) {
+            StringJoiner number = new StringJoiner("");
+            number.add(filaUno.substring(0, THREE))
+                    .add(filaDos.substring(0, THREE))
+                    .add(filaTres.substring(0, THREE));
+
+            String numberContent = numberEqui.get(number.toString());
+            if (numberContent == null) {
+                onlyNumber.add("?");
+            } else {
+                modEleven += Integer.parseInt(numberContent) * i;
+                onlyNumber.add(numberContent);
+            }
+
+            filaUno = filaUno.substring(THREE);
+            filaDos = filaDos.substring(THREE);
+            filaTres = filaTres.substring(THREE);
+        }
+
+        String result = onlyNumber.toString();
+        if (onlyNumber.toString().contains("?")) {
+            result = onlyNumber.toString().concat(" ILL");
+        } else if (modEleven % ELEVEN != 0) {
+            result = onlyNumber.toString().concat(" ERR");
+        }
+        return result;
+    }
 }
--- src/test/java/org/fundacionjala/coding/maria/Kata8Test.java
+++ src/test/java/org/fundacionjala/coding/maria/Kata8Test.java
@@ -1,5 +1,69 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.maria;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Kata 8.
+ */
 public class Kata8Test {
+    private Kata8 kata8;
 
+    /**
+     * initialize kata8.
+     */
+    @Before
+    public void initialize() {
+        kata8 = new Kata8();
+    }
+
+    /**
+     * testBankOCRNormal test 1.
+     */
+    @Test
+    public void testBankOCRNormal() {
+        final String filaUno =  "    _  _     _  _  _  _  _ ";
+        final String filaDos =  "  | _| _||_||_ |_   ||_||_|";
+        final String filaTres = "  ||_  _|  | _||_|  ||_| _|";
+
+        assertEquals("123456789", kata8.bankOCR(filaUno, filaDos, filaTres));
+    }
+
+    /**
+     * testBankOCRErr test 2.
+     */
+    @Test
+    public void testBankOCRErr() {
+        final String filaUno =  " _  _     _  _        _  _ ";
+        final String filaDos =  "|_ |_ |_| _|  |  ||_||_||_ ";
+        final String filaTres = "|_||_|  | _|  |  |  | _| _|";
+
+        assertEquals("664371495 ERR", kata8.bankOCR(filaUno, filaDos, filaTres));
+    }
+
+    /**
+     * testBankOCROneNoNumber test 3.
+     */
+    @Test
+    public void testBankOCROneNoNumber() {
+        String filaUno = "    _  _     _  _  _  _  _ ";
+        String filaDos = "  | _| _||_||_ |_   ||_||  ";
+        String filaTres = "  ||_  _|  | _||_|  ||_| _|";
+
+        assertEquals("12345678? ILL", kata8.bankOCR(filaUno, filaDos, filaTres));
+    }
+
+    /**
+     * testBankOCRTwoNoNumbers test 5.
+     */
+    @Test
+    public void testBankOCRTwoNoNumbers() {
+        String filaUno = "    _  _     _  _  _  _  _ ";
+        String filaDos = "  | _| _||_| _ |_   ||_||_|";
+        String filaTres = "  ||_  _|  | _||_|  ||_| _ ";
+
+        assertEquals("1234?678? ILL", kata8.bankOCR(filaUno, filaDos, filaTres));
+    }
 }
