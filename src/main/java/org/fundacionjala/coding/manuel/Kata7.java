1fbf7b02663df3599684d88a0f4b84121e90cdac
manu863018 <manuel.valdez@fundacion-jala.org>
2018-03-11 20:09:21 -0400

    Kata7 first solution.

--- src/main/java/org/fundacionjala/coding/manuel/Kata7.java
+++ src/main/java/org/fundacionjala/coding/manuel/Kata7.java
@@ -1,4 +1,29 @@
 package org.fundacionjala.coding.manuel;
 
+/**
+ * Created by Juan Manuel on 3/11/2018.
+ */
 public class Kata7 {
+
+    private static final int FIZZ = 3;
+    private static final int BUZZ = 5;
+    private static final int FIZZ_BUZZ = 15;
+    /**
+     * Method to change 3 divisors to Fizz, 5 divisors to Buzz and 15 divisors to FizzBuzz.
+     * @param value is the number to change.
+     * @return value, returns the value changed or the same.
+     */
+    public String fizzBuzz(String value) {
+        int valueValue = Integer.parseInt(value);
+        if (valueValue % FIZZ_BUZZ == 0) {
+            return "FizzBuzz";
+        }
+        if (valueValue % BUZZ == 0 || value.contains("5")) {
+            return "Buzz";
+        }
+        if (valueValue % FIZZ == 0 || value.contains("3")) {
+            return "Fizz";
+        }
+        return value;
+    }
 }
--- src/test/java/org/fundacionjala/coding/manuel/Kata7Test.java
+++ src/test/java/org/fundacionjala/coding/manuel/Kata7Test.java
@@ -1,5 +1,47 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.manuel;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+/**
+ * Created by Juan Manuel on 3/11/2018.
+ */
 public class Kata7Test {
+    private Kata7 kata7;
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZ_BUZZ = "FizzBuzz";
+    /**
+     * Method to initialize object.
+     */
+    @Before
+    public void initialize() {
+        kata7 = new Kata7();
+    }
+    /**
+     * First Test fizzBuzz method.
+     */
+    @Test
+    public void test1FizzBuzzKata7() {
+
+        final String[] expectedResult = new String[]{"1", "2", FIZZ, "4", BUZZ};
+        final String[] initialSequence = new String[]{"1", "2", "3", "4", "5"};
+        for (int i = 0; i < expectedResult.length; i++) {
+            assertEquals(expectedResult[i], kata7.fizzBuzz(initialSequence[i]));
+        }
+    }
+
+    /**
+     * Second Test fizzBuzz method.
+     */
+    @Test
+    public void test2FizzBuzzKata7() {
+        final String[] expectedResult = new String[]{"8", FIZZ, BUZZ, "11", FIZZ, FIZZ, "14", FIZZ_BUZZ, "16"};
+        final String[] initialSequence = new String[]{"8", "9", "10", "11", "12", "13", "14", "15", "16"};
+        for (int i = 0; i < expectedResult.length; i++) {
+            assertEquals(expectedResult[i], kata7.fizzBuzz(initialSequence[i]));
+        }
+    }
 
 }
