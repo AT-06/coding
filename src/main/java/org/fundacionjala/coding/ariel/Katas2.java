36bb080298484ccb28934ac4f0343db03ae9cef6
pipo411 <35770134+pipo411@users.noreply.github.com>
2018-03-12 07:50:49 -0400

    Ariel kata2 (#18)
    * kata1 rot a words that are more than five characters.
    
    * kata2, using the function string join, sort, slipt.
    
    * Katas2, using string, join,split.substring
    
    * Kata2, using String Join, Split, String.substring
    
    * kata2, using Split, Substring, sort,reverse order
    
    * Some error, when I try to upload the Kata2
    
    * Fixing upload Kata2
    
    * Kata2 fixed
    
    * Kata2 fixed 3rd version

---
+++ src/main/java/org/fundacionjala/coding/ariel/Katas2.java
@@ -0,0 +1,34 @@
+package org.fundacionjala.coding.ariel;
+
+import java.util.Arrays;
+
+import java.util.StringJoiner;
+
+import static java.util.Collections.reverseOrder;
+
+/**
+ * Kata2__Ariel Gonzales Vargas.
+ */
+public class Katas2 {
+
+    /**
+     * @param text Any String.
+     * @return A sort String.
+     */
+    public String changeWord(String text) {
+        String[] words = text.split(" ");
+        StringJoiner ult = new StringJoiner(" ");
+
+        for (String w : words) {
+            String[] subw = w.split("");
+            if (w.length() >= 2) {
+                Arrays.sort(subw, 1, w.length() - 1, reverseOrder());
+                ult.add(String.join("", subw));
+            } else {
+                ult.add(w);
+            }
+        }
+
+        return ult.toString();
+    }
+}
