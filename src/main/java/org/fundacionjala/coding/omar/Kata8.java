f8866a719fe723ece9ddbf59a13165e965ab48c3
Omar Huanca Sanchez <omar.huanca@fundacion-jala.org>
2018-03-12 12:18:28 -0400

    fixed
    fixed

---
+++ src/main/java/org/fundacionjala/coding/omar/Kata8.java
@@ -0,0 +1,101 @@
+package org.fundacionjala.coding.omar;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class contains a method that prints the numbers greater to 0.
+ * But for multiples of three print 'Fizz' instead of the number.
+ * and for the multiples of five print 'Buzz'.
+ * For numbers which are multiples of both three and five print 'FizzBuzz'.
+ */
+public class Kata8 {
+
+
+    private static final String ONE = "     |  |";
+
+    private static final String TWO = " _  _||_ ";
+
+    private static final String THREE = " _  _| _|";
+
+    private static final String FOUR = "   |_|  |";
+
+    private static final String FIVE = " _ |_  _|";
+
+    private static final String SIX = " _ |_ |_|";
+
+    private static final String SEVEN = " _   |  |";
+
+    private static final String EIGHT = " _ |_||_|";
+
+    private static final String NINE = " _ |_| _|";
+
+    private static final Map<String, Integer> NUMBERS_MAP;
+
+    private static final int NUMBER_1 = 1;
+
+    private static final int NUMBER_2 = 2;
+
+    private static final int NUMBER_3 = 3;
+
+    private static final int NUMBER_4 = 4;
+
+    private static final int NUMBER_5 = 5;
+
+    private static final int NUMBER_6 = 6;
+
+    private static final int NUMBER_7 = 7;
+
+    private static final int NUMBER_8 = 8;
+
+    private static final int NUMBER_9 = 9;
+
+    static {
+        NUMBERS_MAP = new HashMap<>();
+        NUMBERS_MAP.put(ONE, NUMBER_1);
+        NUMBERS_MAP.put(TWO, NUMBER_2);
+        NUMBERS_MAP.put(THREE, NUMBER_3);
+        NUMBERS_MAP.put(FOUR, NUMBER_4);
+        NUMBERS_MAP.put(FIVE, NUMBER_5);
+        NUMBERS_MAP.put(SIX, NUMBER_6);
+        NUMBERS_MAP.put(SEVEN, NUMBER_7);
+        NUMBERS_MAP.put(EIGHT, NUMBER_8);
+        NUMBERS_MAP.put(NINE, NUMBER_9);
+    }
+
+    /**
+     * @param characterToScan This is character will be scan.
+     * @return result
+     */
+    public String parseSingleCharacter(String characterToScan) {
+
+        Integer result = NUMBERS_MAP.get(characterToScan);
+
+        return result == null ? "?" : result.toString();
+    }
+
+    /**
+     * @param row1 this is the 1st row to scan.
+     * @param row2 this is the 2nd row to scan.
+     * @param row3 this is the 3rd row to scan.
+     * @return result
+     */
+    public String convertEntryToNumber(String row1, String row2, String row3) {
+
+        String[] lines = {row1, row2, row3};
+
+        StringBuilder scannedCharacters = new StringBuilder();
+        for (int i = 0; i < lines[0].length(); i += NUMBER_3) {
+            String character = lines[0].substring(i, i + NUMBER_3)
+                    .concat(lines[NUMBER_1].substring(i, i + NUMBER_3))
+                    .concat(lines[NUMBER_2].substring(i, i + NUMBER_3));
+
+            String parsedCharacter = parseSingleCharacter(character);
+            scannedCharacters.append(parsedCharacter);
+        }
+
+        return scannedCharacters.toString();
+    }
+
+
+}
---
+++ src/test/java/org/fundacionjala/coding/omar/Kata8Test.java
@@ -0,0 +1,31 @@
+package org.fundacionjala.coding.omar;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Powered by Omar Limbert Huanca Sanchez - AT06
+ * This class contains UnitTests for Kata1.java.
+ */
+public class Kata8Test {
+    /**
+     * This method is for test convertEntryToNumber(entry) method.
+     * isFizzBuzzNumber( row1, row2, row3 ) => returns "123456789".
+     */
+    @Test
+    public void testConvertEntryToNumberHistoryOne() {
+        Kata8 entryToNumber = new Kata8();
+        String row1 = "    _  _     _  _  _  _  _ ";
+        String row2 = "  | _| _||_||_ |_   ||_||_|";
+        String row3 = "  ||_  _|  | _||_|  ||_| _|";
+
+
+        assertEquals("123456789", entryToNumber.convertEntryToNumber(row1, row2, row3));
+
+
+    }
+
+
+}
