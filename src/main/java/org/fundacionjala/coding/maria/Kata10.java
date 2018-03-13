b9bcfb542887835a472a35f6e843e38756b4ab42
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-03-13 15:24:20 -0400

    kata 10 test

--- src/main/java/org/fundacionjala/coding/maria/Kata10.java
+++ src/main/java/org/fundacionjala/coding/maria/Kata10.java
@@ -1,4 +1,23 @@
 package org.fundacionjala.coding.maria;
 
+import java.util.Arrays;
+import java.util.StringJoiner;
+
+/**
+ * kata 10.
+ */
 public class Kata10 {
+    private static final String BLANK_SPACE = " ";
+
+    /**
+     * @param numbers numbers
+     * @return hig and low number
+     */
+    public String highAndLow(String numbers) {
+        StringJoiner test = new StringJoiner(BLANK_SPACE);
+        int[] array = Arrays.stream(numbers.split(BLANK_SPACE)).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(array);
+        test.add(Integer.toString(array[array.length - 1])).add(Integer.toString(array[0]));
+        return test.toString();
+    }
 }
