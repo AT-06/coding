72d45d04c6b809309db8f8b340fc2cfafdce76c8
manu863018 <Manuel.Valdez@fundacion-jala.org>
2018-03-13 20:48:58 +0000

    Kata11 first solution. (reverted from commit 531acf6249d57f3cfb46cb00463a177df6678e9a)

--- src/main/java/org/fundacionjala/coding/manuel/Kata11.java
+++ src/main/java/org/fundacionjala/coding/manuel/Kata11.java
@@ -4,22 +4,4 @@ package org.fundacionjala.coding.manuel;
  * Created by Administrator on 3/13/2018.
  */
 public class Kata11 {
-    private static final int PERCENTAJE = 100;
-
-    /**
-     * Method to determine the days until evapor will be usefull.
-     * @param content .
-     * @param evapPerDay .
-     * @param threshold .
-     * @return c days to evapor.
-     */
-    public int evaporator(double content, double evapPerDay, double threshold) {
-        double percent = content * (threshold / PERCENTAJE);
-        int c = 0;
-        while (content > percent) {
-            content -= content * (evapPerDay / PERCENTAJE);
-            c++;
-        }
-        return c;
-    }
 }
