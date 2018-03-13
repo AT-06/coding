dcb555df8f1a255aee64938dfec26b15ecb7a604
Daniel Caballero <daniel.caballero@fundacion-jala.org>
2018-03-12 21:18:47 -0400

    Fixed Kata 11

--- src/main/java/org/fundacionjala/coding/daniel/Kata11.java
+++ src/main/java/org/fundacionjala/coding/daniel/Kata11.java
@@ -4,6 +4,9 @@ package org.fundacionjala.coding.daniel;
  * A class which contains evaporator method.
  */
 public class Kata11 {
+
+    private static final double ONE_HUNDRED = 100.0;
+
     /**
      * @param content the content in ml.
      * @param evapPerDay the percentage of foam or gas lost every day.
@@ -11,6 +14,6 @@ public class Kata11 {
      * @return the nth day (as an integer) on which the evaporator will be out of use.
      */
     public int evaporator(double content, double evapPerDay, double threshold) {
-        return (int) Math.ceil(Math.log(threshold / 100.0) / Math.log(1.0 - evapPerDay / 100.0));
+        return (int) Math.ceil(Math.log(threshold / ONE_HUNDRED) / Math.log(1.0 - evapPerDay / ONE_HUNDRED));
     }
 }
--- src/test/java/org/fundacionjala/coding/daniel/Kata11Test.java
+++ src/test/java/org/fundacionjala/coding/daniel/Kata11Test.java
@@ -2,7 +2,8 @@ package org.fundacionjala.coding.daniel;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * A class for Kata 11 testing.
@@ -25,9 +26,12 @@ public class Kata11Test {
     public void testEvaporator() {
         final int content = 100;
         final int ten = 10;
-        assertEquals(22, kata.evaporator(ten, ten, ten));
-        assertEquals(29, kata.evaporator(ten, ten, 5));
-        assertEquals(59, kata.evaporator(content, 5, 5));
+        final int expected1 = 22;
+        assertEquals(expected1, kata.evaporator(ten, ten, ten));
+        final int expected2 = 29;
+        assertEquals(expected2, kata.evaporator(ten, ten, 5));
+        final int expected3 = 59;
+        assertEquals(expected3, kata.evaporator(content, 5, 5));
         assertEquals(37, kata.evaporator(50, 12, 1));
         assertEquals(31, kata.evaporator(47.5, 8, 8));
         assertEquals(459, kata.evaporator(content, 1, 1));
