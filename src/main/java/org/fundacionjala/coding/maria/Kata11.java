1296ca22495f36a295e2f85ae0eac899af45d1db
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-03-14 12:53:37 -0400

    kata 11 solution

--- src/main/java/org/fundacionjala/coding/maria/Kata11.java
+++ src/main/java/org/fundacionjala/coding/maria/Kata11.java
@@ -1,4 +1,23 @@
 package org.fundacionjala.coding.maria;
 
+/**
+ * kata 11.
+ */
 public class Kata11 {
+    private static final int HUNDRED = 100;
+    /**
+     * @param content contenido ml
+     * @param evapPerDay evaporacion por dia
+     * @param threshold umbral
+     * @return dias
+     */
+    public int evaporator(double content, double evapPerDay, double threshold) {
+        threshold = (threshold * content) / HUNDRED;
+        int dias = 0;
+        while (content > threshold) {
+            content = content - ((evapPerDay * content) / HUNDRED);
+            dias++;
+        }
+        return dias;
+    }
 }
