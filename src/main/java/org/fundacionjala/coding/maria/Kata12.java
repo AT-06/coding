9fb2a874823a20e5892fb354c27342749e5e556e
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-03-14 18:34:42 -0400

    kata 12 2

--- src/main/java/org/fundacionjala/coding/maria/Kata12.java
+++ src/main/java/org/fundacionjala/coding/maria/Kata12.java
@@ -1,4 +1,20 @@
 package org.fundacionjala.coding.maria;
 
+/**
+ * kata 12.
+ */
 public class Kata12 {
+    private static final int INI = 50;
+
+    /**
+     * @param num int
+     * @return score
+     */
+    public long getScore(int num) {
+        long t = 0;
+        for (int i = 1; i <= num; i++) {
+            t = t + (i * INI);
+        }
+        return t;
+    }
 }
