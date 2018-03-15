4a2f71f5c635d40281dccadf85ca4129e8cc6e16
EscarlethFatima <Escarleth.Ledezma@fundacion-jala.org>
2018-03-15 12:12:08 +0000

    Escarleth kata8

--- src/main/java/org/fundacionjala/coding/escarleth/Kata8.java
+++ src/main/java/org/fundacionjala/coding/escarleth/Kata8.java
@@ -6,10 +6,10 @@ import java.util.Map;
 
 /**
  * Powered by Escarleth L.
- * This class  contains some methods to find a bank String
+ * This class  contains some methods to convertNumberOCRtoNumber.
  */
 public class Kata8 {
-    private static Map<String, String> numbers = new HashMap<>();
+    private static final Map<String, String> NUMBERS = new HashMap<>();
     private static final int MOD_ELEVEN = 11;
     private static final int INCREMENT = 3;
     private static final int TAM_MAXIM = 27;
@@ -19,16 +19,16 @@ public class Kata8 {
      * static numbers initializer.
      */
     static {
-        numbers.put(" _ | ||_|", "0");
-        numbers.put("     |  |", "1");
-        numbers.put(" _  _||_ ", "2");
-        numbers.put(" _  _| _|", "3");
-        numbers.put("   |_|  |", "4");
-        numbers.put(" _ |_  _|", "5");
-        numbers.put(" _ |_ |_|", "6");
-        numbers.put(" _   |  |", "7");
-        numbers.put(" _ |_||_|", "8");
-        numbers.put(" _ |_| _|", "9");
+        NUMBERS.put(" _ | ||_|", "0");
+        NUMBERS.put("     |  |", "1");
+        NUMBERS.put(" _  _||_ ", "2");
+        NUMBERS.put(" _  _| _|", "3");
+        NUMBERS.put("   |_|  |", "4");
+        NUMBERS.put(" _ |_  _|", "5");
+        NUMBERS.put(" _ |_ |_|", "6");
+        NUMBERS.put(" _   |  |", "7");
+        NUMBERS.put(" _ |_||_|", "8");
+        NUMBERS.put(" _ |_| _|", "9");
     }
 
     /**
@@ -37,7 +37,7 @@ public class Kata8 {
      * @param thirdRow  is the third String row.
      * @return a transform number.
      */
-    public String bank(String firstRow, String secondRow, String thirdRow) {
+    public String convertNumberOCRtoNumber(String firstRow, String secondRow, String thirdRow) {
         StringBuilder evaluatedNumber = new StringBuilder();
         for (int i = 0; i < TAM_MAXIM; i += SUM_THREE) {
             String number = firstRow.substring(i, i + INCREMENT)
@@ -45,18 +45,18 @@ public class Kata8 {
                     .concat(thirdRow.substring(i, i + INCREMENT));
             evaluatedNumber.append(getNumber(number));
         }
-        return evaluatingResult(evaluatedNumber.toString());
+        return evaluatedNumber.toString();
     }
 
     /**
      * @param number is a number or error number.
      * @return result the evaluating of the number + suffix.
      */
-    private String evaluatingResult(String number) {
+    public String evaluatingResult(String number) {
         if (number.contains("?")) {
             return addSuffix(number);
         }
-        return verifyModuleEleven(number) ? number : addSuffix(number);
+        return isValidCheckSum(number) ? number : addSuffix(number);
     }
 
     /**
@@ -64,18 +64,18 @@ public class Kata8 {
      * @return result a positive number
      */
     private String getNumber(String number) {
-        return numbers.get(number) != null ? numbers.get(number) : "?";
+        return NUMBERS.getOrDefault(number, "?");
     }
 
     /**
      * @param number is a number.
-     * @return if the number%11=0.
+     * @return if the sum%11=0.
      */
-    private boolean verifyModuleEleven(String number) {
+    private boolean isValidCheckSum(String number) {
         int sum = 0;
         int cont = 0;
         for (int i = number.length() - 1; i > 0; i--) {
-            sum += ((int) number.charAt(cont)) * i;
+            sum += Character.getNumericValue(number.charAt(cont)) * i;
             cont++;
         }
         return sum % MOD_ELEVEN == 0;
--- src/test/java/org/fundacionjala/coding/escarleth/Kata8Test.java
+++ src/test/java/org/fundacionjala/coding/escarleth/Kata8Test.java
@@ -11,10 +11,8 @@ import org.junit.Test;
 
 public class Kata8Test {
     private Kata8 kata8;
-    private static final String COMMON_LINE1 = " _  _     _  _        _  _ ";
-    private static final String COMMON_LINE2 = "    _  _     _  _  _  _  _ ";
-    private static final String COMMON_LINE3 = "  |  |  |  |  |  |  |  |  |";
-    private static final String COMMON_LINE4 = "  ||_  _|  | _||_|  ||_| _|";
+    private static final String COMMON_LINE1 = "    _  _     _  _  _  _  _ ";
+    private static final String COMMON_LINE2 = "  ||_  _|  | _||_|  ||_| _|";
 
     /**
      * initialize kata8.
@@ -25,53 +23,38 @@ public class Kata8Test {
     }
 
     /**
-     * BankKata test1 expected 123456789.
+     * testConvertValidNumberOCRtoNumber return the correct number.
      */
     @Test
-    public void test1BankKata8() {
+    public void testConvertValidNumberOCRtoNumber() {
         String secondRow = "  | _| _||_||_ |_   ||_||_|";
-        assertEquals("123456789", kata8.bank(COMMON_LINE2, secondRow, COMMON_LINE4));
+        assertEquals("123456789", kata8.convertNumberOCRtoNumber(COMMON_LINE1, secondRow, COMMON_LINE2));
     }
 
     /**
-     * BankKata test expected 12345678? ILL.
+     * testConvertInvalidNumberOCRtoNumber return the incorrect String.
      */
     @Test
-    public void test2BankKata8() {
-        String secondRow = "  | _| _||_||_ |_   ||_||  ";
-        assertEquals("12345678? ILL", kata8.bank(COMMON_LINE2, secondRow, COMMON_LINE4));
+    public void testConvertInvalidNumberOCRtoNumber() {
+        String secondRow = "  | _  _||_||_ |_   ||_||_|";
+        assertEquals("1?3456789", kata8.convertNumberOCRtoNumber(COMMON_LINE1, secondRow, COMMON_LINE2));
+
     }
 
     /**
-     * BankKata test expected 664371495 ERR.
+     * testEvaluatingResultToNumberWithLLL return the number added sufFix ILL.
      */
     @Test
-    public void tes3BankKata8() {
-
-        String secondRow = "|_ |_ |_| _|  |  ||_||_||_ ";
-        String thirdRow = "|_||_|  | _|  |  |  | _| _|";
-
-        assertEquals("664371495 ERR", kata8.bank(COMMON_LINE1, secondRow, thirdRow));
+    public void testEvaluatingResultToNumberWithILL() {
+        assertEquals("12345?78? ILL", kata8.evaluatingResult("12345?78?"));
     }
 
     /**
-     * BankKata test 1234?678? ILL.
+     * testEvaluatingResultToNumberWithLLL return the number added sufFix ERR.
      */
     @Test
-    public void test4BankKata8() {
-        String secondRow = "  | _| _||_| _ |_   ||_||_|";
-        String thirdRow = "  ||_  _|  | _||_|  ||_| _ ";
-
-        assertEquals("1234?678? ILL", kata8.bank(COMMON_LINE2, secondRow, thirdRow));
-    }
-
-    /**
-     * BankKata test 111111111 ERR.
-     */
-    @Test
-    public void test5BankKata8() {
-        String firstRow = "                           ";
-        assertEquals("111111111 ERR", kata8.bank(firstRow, COMMON_LINE3, COMMON_LINE3));
+    public void testEvaluatingResultToNumberWithERR() {
+        assertEquals("664371495 ERR", kata8.evaluatingResult("664371495"));
     }
 }
 
