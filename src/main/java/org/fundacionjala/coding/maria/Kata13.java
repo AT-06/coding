fd064d74713f4d2c3701d10845493bad03f02117
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-03-15 21:52:19 -0400

    kata 13 solution

--- src/main/java/org/fundacionjala/coding/maria/Kata13.java
+++ src/main/java/org/fundacionjala/coding/maria/Kata13.java
@@ -1,4 +1,23 @@
 package org.fundacionjala.coding.maria;
 
+/**
+ * Kata 13, validate number.
+ */
 public class Kata13 {
+    private static final int TEN = 10;
+    private static final int THREE = 10;
+    /**
+     * @param number string number to validate
+     * @return boolean if the number is valid
+     */
+    public boolean validate(String number) {
+        String[] numberArray = number.split("");
+        int total = 0;
+        for (int i = 0; i < numberArray.length - 1; i += 2) {
+            total += Integer.parseInt(numberArray[i]);
+            total += i < numberArray.length - 2 ? Integer.parseInt(numberArray[i + 1]) * THREE : 0;
+        }
+        total = TEN - (total % TEN);
+        return total == Integer.parseInt(numberArray[numberArray.length - 1]);
+    }
 }
--- src/test/java/org/fundacionjala/coding/maria/Kata13Test.java
+++ src/test/java/org/fundacionjala/coding/maria/Kata13Test.java
@@ -1,5 +1,31 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.maria;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * test for kata 13.
+ */
 public class Kata13Test {
+    private Kata13 eanValidator;
+    /**
+     * initialize kata7.
+     */
+    @Before
+    public void initialize() {
+        eanValidator = new Kata13();
+    }
 
+    /**
+     * test cases for eanValidator.
+     */
+    @Test
+    public void eanValidatorTest() {
+
+        assertTrue(eanValidator.validate("4003301018398"));
+        assertFalse(eanValidator.validate("4003301018392"));
+    }
 }
