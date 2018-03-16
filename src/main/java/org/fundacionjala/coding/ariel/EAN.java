75a50cc2d072012471afd0f342678990537a009f
pipo411 <Ariel.Gonzales@fundacion-jala.org>
2018-03-15 22:01:28 -0400

    Kata13 uploaded

--- src/main/java/org/fundacionjala/coding/ariel/EAN.java
+++ src/main/java/org/fundacionjala/coding/ariel/EAN.java
@@ -1,4 +1,29 @@
 package org.fundacionjala.coding.ariel;
 
+/**
+ * Ean checksum.
+ * By Ariel Gonzales..
+ */
 public class EAN {
+
+    public static final int LASTDIGIT = 12;
+    public static final int PAR = 3;
+    public static final int CHECKSUM1 = 10;
+
+    /**
+     * @param number A string number.
+     * @return if the checksum is validate.
+     */
+    public boolean validate(String number) {
+        String[] numeros = number.split("");
+        int condition = Integer.parseInt(numeros[LASTDIGIT]);
+        int suma = 0;
+        int checksum = 0;
+        for (int i = 0; i < numeros.length - 1; i++) {
+            suma = (i % 2 == 0) ? suma + Integer.parseInt(numeros[i]) * 1 : suma + Integer.parseInt(numeros[i]) * PAR;
+        }
+        checksum = CHECKSUM1 - (suma % CHECKSUM1);
+        return checksum == condition;
+    }
 }
+
--- src/test/java/org/fundacionjala/coding/ariel/EANTest.java
+++ src/test/java/org/fundacionjala/coding/ariel/EANTest.java
@@ -1,5 +1,22 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.ariel;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unittests.
+ */
 public class EANTest {
-
+    /**
+     * This test verify if the plus number..
+     * is validate for its checksum.
+     */
+    @Test
+    public void testEanValidate() {
+        EAN eanvalidator = new EAN();
+        assertTrue(eanvalidator.validate("4003301018398"));
+        assertFalse(eanvalidator.validate("4003301018392"));
+    }
 }
