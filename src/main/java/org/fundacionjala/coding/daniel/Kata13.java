8aa61db942a3b93fd3d2b2e51553de7b724d6d11
Daniel Caballero <daniel.caballero@fundacion-jala.org>
2018-03-16 09:51:52 -0400

    Fixed Kata 13

--- src/main/java/org/fundacionjala/coding/daniel/Kata13.java
+++ src/main/java/org/fundacionjala/coding/daniel/Kata13.java
@@ -4,21 +4,20 @@ package org.fundacionjala.coding.daniel;
  * A class which contains validate method.
  */
 public class Kata13 {
-    private int factor;
 
     /**
      * @param eanCode EAN is a 13-digits barcode consisting of 12-digits data followed by a single-digit checksum.
      * @return Return true if the given EAN-Code is valid, otherwise false.
      */
     public boolean validate(final String eanCode) {
-        factor = 1;
+        final int[] factor = {1};
         int checksum = eanCode.substring(0, eanCode.length() - 1)
             .chars()
             .map(i -> Character.getNumericValue((char) i))
             .map(i -> {
-                int result = i * factor;
+                int result = i * factor[0];
                 final int multiplier = 3;
-                factor = factor == 1 ? multiplier : 1;
+                factor[0] = factor[0] == 1 ? multiplier : 1;
                 return result;
             })
             .sum();
