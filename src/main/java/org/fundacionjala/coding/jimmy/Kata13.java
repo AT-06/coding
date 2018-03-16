fb222b71e481807a2f0a051074b275734762bb04
JimmyRomero <jimmy.romero@fundacion-jala.org>
2018-03-15 20:02:49 -0400

    Kata13 developed

---
+++ src/main/java/org/fundacionjala/coding/jimmy/Kata13.java
@@ -0,0 +1,29 @@
+package org.fundacionjala.coding.jimmy;
+
+/**
+ * Class created in order to develop validate method.
+ */
+public class Kata13 {
+
+    public static final int NUMBER_THREE = 3;
+    public static final int NUMBER_TEN = 10;
+
+    /**
+     * It validates a number in order to know whether is a EAN number.
+     *
+     * @param eanNumber is the number to validate.
+     * @return the validation
+     */
+    public boolean validate(String eanNumber) {
+        int sum = 0;
+        int lastDigit = Integer.parseInt(eanNumber.substring(eanNumber.length() - 1));
+
+        for (int i = 1; i < eanNumber.length(); i++) {
+            int currentIntFromString = Integer.parseInt(eanNumber.substring(i - 1, i));
+            sum += i % 2 == 0 ? NUMBER_THREE * currentIntFromString : currentIntFromString;
+        }
+
+        sum = sum % NUMBER_TEN == 0 ? 0 : NUMBER_TEN - (sum % NUMBER_TEN);
+        return sum == lastDigit;
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/jimmy/Kata13Test.java
@@ -0,0 +1,39 @@
+package org.fundacionjala.coding.jimmy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * This class is created in order to test the validate method.
+ */
+public class Kata13Test {
+
+    private Kata13 kata13;
+
+    /**
+     * Initializes kata13.
+     */
+    @Before
+    public void initialize() {
+        kata13 = new Kata13();
+    }
+
+    /**
+     * It validates a valid EAN number.
+     */
+    @Test
+    public void testValidateValidEANNumber() {
+        assertTrue(kata13.validate("4003301018398"));
+    }
+
+    /**
+     * It does not validate a invalid EAN number.
+     */
+    @Test
+    public void testValidateInvalidEANNumber() {
+        assertFalse(kata13.validate("4003301018392"));
+    }
+}
