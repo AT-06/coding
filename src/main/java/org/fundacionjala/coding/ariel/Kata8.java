62433ad85b2cfcb8d0ff8e362c34beb590eeb5d9
pipo411 <Ariel.Gonzales@fundacion-jala.org>
2018-03-18 18:03:33 -0400

    Kata8 solved

---
+++ src/main/java/org/fundacionjala/coding/ariel/Kata8.java
@@ -0,0 +1,77 @@
+package org.fundacionjala.coding.ariel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+/**
+ * Kata 8.
+ * By Ariel Gonzales Vargas.
+ */
+public class Kata8 {
+
+    public static final int CHECKSUM = 11;
+    public static final int THREE = 3;
+    private static final Map<String, String> LIST = new HashMap<>();
+    public static final int NUMBER = 9;
+
+    static {
+        LIST.put(" _ | ||_|", "0");
+        LIST.put("     |  |", "1");
+        LIST.put(" _  _||_ ", "2");
+        LIST.put(" _  _| _|", "3");
+        LIST.put("   |_|  |", "4");
+        LIST.put(" _ |_  _|", "5");
+        LIST.put(" _ |_ |_|", "6");
+        LIST.put(" _   |  |", "7");
+        LIST.put(" _ |_||_|", "8");
+        LIST.put(" _ |_| _|", "9");
+    }
+
+    /**
+     * @param file1 A part of the digit.
+     * @param file2 A part of the digit.
+     * @param file3 A part of the digit.
+     * @return A checksum serie or serie ERR or ILL.
+     */
+    public String story1(String file1, String file2, String file3) {
+        StringJoiner digitSum = new StringJoiner("");
+
+        for (int i = 0; i < file1.length(); i += THREE) {
+            if (i != file1.length()) {
+                String digit = file1.substring(i, i + THREE)
+                        .concat(file2.substring(i, i + THREE))
+                        .concat(file3.substring(i, i + THREE));
+                digitSum.add(LIST.containsKey(digit) ? LIST.get(digit) : "?");
+            }
+        }
+
+        return digitSum.toString();
+    }
+
+    /**
+     * @param serie A  string serie.
+     * @return a serie ILL or ERR.
+     */
+    public String valid(String serie) {
+        if (serie.contains("?")) {
+            return String.format("%s %s", serie, "ILL");
+        }
+        return checkSumCheck(serie) ? serie : String.format("%s %s", serie, "ERR");
+    }
+
+    /**
+     * @param serie A  string serie.
+     * @return a checksum serie or not.
+     */
+    public boolean checkSumCheck(String serie) {
+        int number = NUMBER;
+        int sum = 0;
+        for (int i = 0; i < serie.length(); i++) {
+            int checks = Character.getNumericValue(serie.charAt(i)) * number;
+            sum += checks;
+            number--;
+        }
+        return sum % CHECKSUM == 0;
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/ariel/Kata8Test.java
@@ -0,0 +1,97 @@
+package org.fundacionjala.coding.ariel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Unittets Kata Bank.
+ */
+public class Kata8Test {
+    private Kata8 katita;
+
+    /**
+     * Init the object Kata8.
+     */
+    @Before
+    public void init() {
+        katita = new Kata8();
+    }
+
+    /**
+     * This test check the story1.
+     */
+    @Test
+    public void testStory1Serie() {
+        final String underscores = "    _  _     _  _  _  _  _ ";
+        final String pipesUnderscores = "  | _| _||_||_ |_   ||_||_|";
+        final String underscoresPipes = "  ||_  _|  | _||_|  ||_| _|";
+        assertEquals("123456789", katita.story1(underscores, pipesUnderscores, underscoresPipes));
+
+    }
+
+    /**
+     * This test check the story1.
+     * But other serie.
+     */
+    @Test
+    public void testStory1Serie2() {
+        final String commonLine1 = " _  _  _  _  _  _  _  _  _ ";
+        final String commonLine2 = "| || || || || || || || || |";
+        final String commonLine3 = "|_||_||_||_||_||_||_||_||_|";
+        assertEquals("000000000", katita.story1(commonLine1, commonLine2, commonLine3));
+    }
+
+    /**
+     * This test check the story1.
+     * With an invalid number.
+     */
+    @Test
+    public void testStory1Serie3() {
+        String commonLine1 = "    _  _     _  _  _  _  _ ";
+        String commonLine2 = "  | _| _||_||_ |_   ||_||  ";
+        String commonLine3 = "  ||_  _|  | _||_|  ||_| _|";
+        assertEquals("12345678?", katita.story1(commonLine1, commonLine2, commonLine3));
+    }
+
+
+    /**
+     * This test check if the serie.
+     * pass the checksum condition.
+     */
+    @Test
+    public void testValidSerie() {
+        assertEquals("777777777 ERR", katita.valid("777777777"));
+    }
+
+    /**
+     * This test check if the serie.
+     * has a non number.
+     */
+    @Test
+    public void testInvalidSerie() {
+        assertEquals("1234?6789 ILL", katita.valid("1234?6789"));
+    }
+
+    /**
+     * This test check if sthe serie.
+     * is invalid for the checksum.
+     */
+    @Test
+    public void testInvalitChecksum() {
+
+        assertFalse(katita.checkSumCheck("345812865"));
+    }
+
+    /**
+     * This test check if sthe serie.
+     * is valid for the checksum.
+     */
+    @Test
+    public void testValidChecksum() {
+        assertTrue(katita.checkSumCheck("123456789"));
+    }
+}
