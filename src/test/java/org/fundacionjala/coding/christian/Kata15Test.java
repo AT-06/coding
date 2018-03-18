bf1c496b5985ff745ef8db315e305f7026a648b2
Christian Galarza <Christian.Galarza@fundacion-jala.org>
2018-03-18 12:48:54 -0400

    Christian_kata15
    Kata15 test fixed

--- src/test/java/org/fundacionjala/coding/christian/Kata15Test.java
+++ src/test/java/org/fundacionjala/coding/christian/Kata15Test.java
@@ -10,17 +10,6 @@ import static org.junit.Assert.assertEquals;
 public class Kata15Test {
 
     private Kata15 kata15;
-    private static final int[] ACTUAL = {3, -1, -1};
-    private static final int EXPECTED_1 = 2;
-    private static final int[] ACTUAL_2 = {3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3};
-    private static final int EXPECTED_2 = 5;
-    private static final int[] ACTUAL_3 = {9};
-    private static final int EXPECTED_3 = 1;
-    private static final int[] ACTUAL_4 = {7, 1, 7, 1, 7, 1};
-    private static final int EXPECTED_4 = 3;
-    private static final int[] ACTUAL_5 = {};
-    private static final int EXPECTED_5 = 0;
-
 
     /**
      *
@@ -36,9 +25,13 @@ public class Kata15Test {
      */
     @Test
     public void basicTests() {
+        final int[] actual = {3, -1, -1};
+        final int expected1 = 2;
+        final int[] actual2 = {3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3};
+        final int expected2 = 5;
         String msg = "Should work with example test cases";
-        assertEquals(msg, EXPECTED_1, kata15.mostFrequentItemCount(ACTUAL));
-        assertEquals(msg, EXPECTED_2, kata15.mostFrequentItemCount(ACTUAL_2));
+        assertEquals(msg, expected1, kata15.mostFrequentItemCount(actual));
+        assertEquals(msg, expected2, kata15.mostFrequentItemCount(actual2));
     }
 
     /**
@@ -46,10 +39,17 @@ public class Kata15Test {
      */
     @Test
     public void edgeTests() {
-        assertEquals("Should work for 1-element arrays", EXPECTED_3, kata15.mostFrequentItemCount(ACTUAL_3));
+
+        final int[] actual3 = {9};
+        final int expected3 = 1;
+        final int[] actual4 = {7, 1, 7, 1, 7, 1};
+        final int expected4 = 3;
+        final int[] actual5 = {};
+        final int expected5 = 0;
+        assertEquals("Should work for 1-element arrays", expected3, kata15.mostFrequentItemCount(actual3));
         assertEquals("Should work with multiple most frequent items, e.g. nine 7's and nine 1's",
-                EXPECTED_4, kata15.mostFrequentItemCount(ACTUAL_4));
-        assertEquals("Should work for empty arrays", EXPECTED_5, kata15.mostFrequentItemCount(ACTUAL_5));
+                expected4, kata15.mostFrequentItemCount(actual4));
+        assertEquals("Should work for empty arrays", expected5, kata15.mostFrequentItemCount(actual5));
         }
 }
 
