d4c032fa8224c40df386f257d9225bebf87333bd
mrcm15 <mariarenee.canqui@gmail.com>
2018-03-18 17:59:11 -0400

    kata 14 changes requested

--- src/main/java/org/fundacionjala/coding/maria/Kata14.java
+++ src/main/java/org/fundacionjala/coding/maria/Kata14.java
@@ -10,13 +10,14 @@ public class Kata14 {
      * @return averages of the numbers
      */
     public double[] averages(int[] numberArray) {
-        if (numberArray != null && numberArray.length > 1) {
-                double[] avera = new double[numberArray.length - 1];
-                for (int i = 0; i < numberArray.length - 1; i++) {
-                    avera[i] = (numberArray[i] + numberArray[i + 1]) / DIV;
-                }
-                return avera;
+        
+        if (numberArray == null || numberArray.length == 0) {
+            return new double[]{};
         }
-        return new double[]{};
+        double[] average = new double[numberArray.length - 1];
+        for (int i = 0; i < numberArray.length - 1; i++) {
+            average[i] = (numberArray[i] + numberArray[i + 1]) / DIV;
+        }
+        return average;
     }
 }
