989f2f3ee5d885b31e0679488fdf5862a3c41d97
Christian Galarza <Christian.Galarza@fundacion-jala.org>
2018-03-17 21:32:55 -0400

    Kata13 corrected

--- src/main/java/org/fundacionjala/coding/christian/ChristianKata13.java
+++ src/main/java/org/fundacionjala/coding/christian/ChristianKata13.java
@@ -4,21 +4,25 @@ package org.fundacionjala.coding.christian;
  * Performed by Christian.
  */
 public class ChristianKata13 {
+    static final int NUMBER_12 = 12;
+    static final int NUMBER_3 = 3;
+    static final int NUMBER_10 = 10;
+
 
     /**
      * @param eanCode this is for validate
      * @return tag for 'eanCode'
      */
     public boolean validate(final String eanCode) {
-        int expected = Character.getNumericValue(eanCode.charAt(12));
+        int expected = Character.getNumericValue(eanCode.charAt(NUMBER_12));
         int sum = 0;
 
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < NUMBER_12; i++) {
             int digit = Character.getNumericValue(eanCode.charAt(i));
-            sum += ((i + 1) % 2 == 0) ? digit * 3 : digit;
+            sum += ((i + 1) % 2 == 0) ? digit * NUMBER_3 : digit;
         }
-        sum = sum % 10;
+        sum = sum % NUMBER_10;
 
-        return (sum == 0) ? 0 == expected : 10 - sum == expected;
+        return (sum == 0) ? 0 == expected : NUMBER_10 - sum == expected;
     }
 }
