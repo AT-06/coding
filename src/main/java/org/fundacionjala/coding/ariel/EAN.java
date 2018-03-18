ec657b86fbcbb4180756da2a2c4aa9ad80471af5
pipo411 <35770134+pipo411@users.noreply.github.com>
2018-03-17 20:13:20 -0400

    Ariel Kata13 (#115)
    * kata1 rot a words that are more than five characters.
    
    * kata2, using the function string join, sort, slipt.
    
    * Katas2, using string, join,split.substring
    
    * Kata2, using String Join, Split, String.substring
    
    * kata2, using Split, Substring, sort,reverse order
    
    * Some error, when I try to upload the Kata2
    
    * Fixing upload Kata2
    
    * Kata2 fixed
    
    * Kata7 solved using IF
    
    * Kata7 upload again solved by IF
    
    * Kata13 solved
    
    * Kata13 uploaded
    
    * Kata13 fixed
    
    * Kata 13 fixed
    
    * Kata 13 fixed

---
+++ src/main/java/org/fundacionjala/coding/ariel/EAN.java
@@ -0,0 +1,28 @@
+package org.fundacionjala.coding.ariel;
+
+/**
+ * Ean checksum.
+ * By Ariel Gonzales.
+ */
+public class EAN {
+
+    public static final int LASTDIGIT = 12;
+    public static final int PAR = 3;
+    public static final int CHECKSUM1 = 10;
+
+    /**
+     * @param number A string number.
+     * @return if the checksum is validate.
+     */
+    public boolean validate(String number) {
+        String[] numeros = number.split("");
+        int suma = 0;
+        for (int i = 0; i < numeros.length - 1; i++) {
+            int num = Integer.parseInt(numeros[i]);
+            suma += (i % 2 == 0) ? num : num * PAR;
+        }
+        int checksum = (suma % CHECKSUM1 == 0) ? 0 : CHECKSUM1 - (suma % CHECKSUM1);
+        return checksum == Integer.parseInt(numeros[LASTDIGIT]);
+    }
+}
+
