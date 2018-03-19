c4b3d676c00bb99273b9e9bfda62af91fdfa9e1e
mery <mariarenee.canqui@gmail.com>
2018-03-19 09:10:04 -0400

    Maria kata10 (#93)
    * kata 10 solution
    
    * kata 10 test
    
    * Kata 10 test changes
    
    * kata 10 changes requested
    
    * kata 10 remove test
    
    * kata 10 changes 2
    
    * Kata 10 , string format added
    
    * kata 10, string format added
    
    * kata 10 return added
    
    * kata 10 changes 3
    
    * kata 10 removed import
    
    * Update Kata10.java

---
+++ src/main/java/org/fundacionjala/coding/maria/Kata10.java
@@ -0,0 +1,20 @@
+package org.fundacionjala.coding.maria;
+
+import java.util.Arrays;
+
+/**
+ * kata 10.
+ */
+public class Kata10 {
+    private static final String BLANK_SPACE = " ";
+
+    /**
+     * @param numbers numbers
+     * @return hig and low number
+     */
+    public String highAndLow(String numbers) {
+        int[] array = Arrays.stream(numbers.split(BLANK_SPACE)).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(array);
+        return String.format("%s %s", String.valueOf(array[array.length - 1]), String.valueOf(array[0]));
+    }
+}
