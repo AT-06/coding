7d9c88847f039f6c24b579007052761f35099283
pipo411 <35770134+pipo411@users.noreply.github.com>
2018-03-19 13:02:34 -0400

    Ariel kata8 (#141)
    * kata1 rot a words that are more than five characters.
    
    * kata2, using the function string join, sort, slipt.
    
    * Katas2, using string, join,split.substring
    
    * Kata2, using String Join, Split, String.substring
    
    * kata2, using Split, Substring, sort,reverse order
    
    * Some error, when I try to upload the Kata2
    
    * Fixing upload Kata2
    
    * Kata2 fixed
    
    * Kata2 fixed 3rd version
    
    * Kata8 solved
    
    * Kata8 fixed
    
    * Kata 8 fixed to increased the coverage
    
    * Kata8 fixed
    
    * Kata 8 fixed
    
    * Kata 8 fixed
    
    * Kata8 fixed

---
+++ src/test/java/org/fundacionjala/coding/ariel/Kata8Test.java
@@ -0,0 +1,116 @@
+package org.fundacionjala.coding.ariel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Unittets Kata Bank..
+ */
+public class Kata8Test {
+    private Kata8 katita;
+
+    /**
+     * Init the object Kata8.
+     */
+    @Before
+    public void init() {
+        katita = new Kata8();
+    }
+
+    /**
+     * This test check the story1.
+     */
+    @Test
+    public void testStory1Serie() {
+        final String underscores = "    _  _     _  _  _  _  _ ";
+        final String pipesUnderscores = "  | _| _||_||_ |_   ||_||_|";
+        final String underscoresPipes = "  ||_  _|  | _||_|  ||_||_|";
+        assertEquals("123456788", katita.story1(underscores, pipesUnderscores, underscoresPipes));
+
+    }
+
+    /**
+     * This test check the story1.
+     * But other serie.
+     */
+    @Test
+    public void testStory1Serie2() {
+        final String commonLine1 = " _  _  _  _  _  _  _  _  _ ";
+        final String commonLine2 = "| || || || || || || || || |";
+        final String commonLine3 = "|_||_||_||_||_||_||_||_||_|";
+        assertEquals("000000000", katita.story1(commonLine1, commonLine2, commonLine3));
+    }
+
+    /**
+     * This test check the story1.
+     * With an invalid number.
+     */
+    @Test
+    public void testStory1Serie3() {
+        final String commonLine1 = "    _  _     _  _  _  _  _ ";
+        final String commonLine2 = "  | _| _||_||_ |_   ||_||  ";
+        final String commonLine3 = "  ||_  _|  | _||_|  ||_| _|";
+        assertEquals("12345678?", katita.story1(commonLine1, commonLine2, commonLine3));
+    }
+
+
+    /**
+     * This test check if the serie.
+     * pass the checksum condition.
+     */
+    @Test
+    public void testValidWithoutChecksum() {
+        assertEquals("777777777 ERR", katita.valid("777777777"));
+    }
+
+    /**
+     * This test check if the serie.
+     * pass the checksum condition.
+     */
+    @Test
+    public void testValidWithoutChecksum2() {
+        assertEquals("664371495 ERR", katita.valid("664371495"));
+    }
+
+
+    /**
+     * This test check if the serie.
+     * has a non number.
+     */
+    @Test
+    public void testInvalid() {
+        assertEquals("1234?6789 ILL", katita.valid("1234?6789"));
+    }
+
+    /**
+     * This test check if the serie.
+     * is valid.
+     */
+    @Test
+    public void testValidWithoutSuffix() {
+        assertEquals("345882865", katita.valid("345882865"));
+    }
+
+    /**
+     * This test check if sthe serie.
+     * is invalid for the checksum.
+     */
+    @Test
+    public void testInvalidChecksum() {
+
+        assertFalse(katita.isCheckSum("345812865"));
+    }
+
+    /**
+     * This test check if sthe serie.
+     * is valid for the checksum.
+     */
+    @Test
+    public void testValidChecksum() {
+        assertTrue(katita.isCheckSum("123456789"));
+    }
+}
