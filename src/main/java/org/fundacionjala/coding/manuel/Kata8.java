6071a24c9db5ca91bbab25602ecb096c51774fd5
manu863018 <36738685+manu863018@users.noreply.github.com>
2018-03-19 08:53:20 -0400

    Manuel kata8 (#73)
    * Kata8 first solution.
    
    * Kata8 first solution.
    
    * Kata8 correction first solution.
    
    * Kata8 corrections first solution.
    
    * Kata8 correction first solution.
    
    * Kata8 corrections first solution.
    
    * Kata8 corrections for first solution.
    
    * Kata8 fix unit tests and methods.
    
    * Kata8 fix final strings.
    
    * Kata8 fix static final variables.
    
    * Kata8 fix method return value.
    
    * Kata8 fix unit tests params.

---
+++ src/main/java/org/fundacionjala/coding/manuel/Kata8.java
@@ -0,0 +1,88 @@
+package org.fundacionjala.coding.manuel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+/**
+ * Created by Juan Manuel on 3/11/2018.
+ */
+public class Kata8 {
+
+    private static final int SUFIX = 3;
+    private static final int MOD_NUMBER = 11;
+    private static Map<String, String> mapedValues = new HashMap<>();
+
+    /**
+     * static initializer.
+     */
+    static {
+        mapedValues.put(" _ | ||_|", "0");
+        mapedValues.put("     |  |", "1");
+        mapedValues.put(" _  _||_ ", "2");
+        mapedValues.put(" _  _| _|", "3");
+        mapedValues.put("   |_|  |", "4");
+        mapedValues.put(" _ |_  _|", "5");
+        mapedValues.put(" _ |_ |_|", "6");
+        mapedValues.put(" _   |  |", "7");
+        mapedValues.put(" _ |_||_|", "8");
+        mapedValues.put(" _ |_| _|", "9");
+    }
+
+    /**
+     * Method bank to convert number.
+     * @param firstRow first row of pipes and underscore.
+     * @param secondRow second row of pipes and underscore.
+     * @param thirdRow third row of pipes and underscore.
+     * @return numberValue converted.
+     */
+    public String convertEntryToNumber(String firstRow, String secondRow, String thirdRow) {
+        StringJoiner numberValue = new StringJoiner("");
+        while (!firstRow.isEmpty()) {
+            String concatenatedNumber = firstRow.substring(0, SUFIX)
+                    .concat(secondRow.substring(0, SUFIX))
+                    .concat(thirdRow.substring(0, SUFIX));
+            numberValue.add(mapedValues.getOrDefault(concatenatedNumber, "?"));
+            firstRow = firstRow.substring(SUFIX);
+            secondRow = secondRow.substring(SUFIX);
+            thirdRow = thirdRow.substring(SUFIX);
+        }
+        return numberValue.toString();
+    }
+
+    /**
+     * Method evaluate if the account is ok.
+     * @param numberValue number converted.
+     * @return string value account if ? then ILL, not check sum then ERR.
+     */
+    public String evaluateAccount(String numberValue) {
+        if (numberValue.contains("?")) {
+            return addSufixes(numberValue);
+        }
+        return validCheckSum(numberValue) ? numberValue : addSufixes(numberValue);
+    }
+
+    /**
+     * Method addSufixes to add ILL in case ?.
+     * @param numberValue number converted.
+     * @return string value account if ? then ILL.
+     */
+    public String addSufixes(String numberValue) {
+        return numberValue.contains("?") ? numberValue.concat(" ILL") : numberValue.concat(" ERR");
+    }
+
+    /**
+     * Method calculateCheckSum to check if it is a valid account.
+     * @param number number converted.
+     * @return string value account if not real then concat ERR.
+     */
+    public boolean validCheckSum(String number) {
+        int sum = 0;
+        String numberCopy = number;
+        for (int i = numberCopy.length(); i > 0; i--) {
+            sum += Integer.parseInt(numberCopy.substring(0, 1)) * i;
+            numberCopy = numberCopy.substring(1);
+        }
+        return sum % MOD_NUMBER == 0;
+    }
+}
