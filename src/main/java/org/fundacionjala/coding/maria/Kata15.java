ca1e4f1d4daf62b1a5c677f4aca1fba60c5280e4
mrcm15 <mariarenee.canqui@gmail.com>
2018-03-19 00:36:00 -0400

    kata 15 second sol, return correct.

--- src/main/java/org/fundacionjala/coding/maria/Kata15.java
+++ src/main/java/org/fundacionjala/coding/maria/Kata15.java
@@ -1,4 +1,24 @@
 package org.fundacionjala.coding.maria;
 
+/**
+ * Kata 15.
+ */
 public class Kata15 {
+    /**
+     * @param arrayNumber receive an array
+     * @return return the number of repeticions of the most frecuent number
+     */
+    public int mostFrequentItemCount(int[] arrayNumber) {
+        int mostFqNumber = 0;
+        for (int i = 0; i < arrayNumber.length; i++) {
+            int count = 0;
+            for (int number : arrayNumber) {
+                if (number == arrayNumber[i]) {
+                    count++;
+                }
+            }
+            mostFqNumber = count > mostFqNumber ? count : mostFqNumber;
+        }
+        return mostFqNumber;
+    }
 }
--- src/test/java/org/fundacionjala/coding/maria/Kata15Test.java
+++ src/test/java/org/fundacionjala/coding/maria/Kata15Test.java
@@ -1,5 +1,38 @@
+package org.fundacionjala.coding.maria;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class Kata15Test {
+    private Kata15 kata15;
+    /**
+     * initialize kata15.
+     */
+    @Before
+    public void initialize() {
+        kata15 = new Kata15();
+    }
+
+    /**
+     * test basicTests.
+     */
+    @Test
+    public void basicTests() {
+        String msg = "Should work with example test cases";
+        assertEquals(msg, 2, kata15.mostFrequentItemCount(new int[] {3, -1, -1}));
+        assertEquals(msg, 5, kata15.mostFrequentItemCount(new int[] {3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3}));
+    }
+
+    /**
+     * test edgeTests.
+     */
+    @Test
+    public void edgeTests() {
+        assertEquals("Should work for empty arrays", 0, kata15.mostFrequentItemCount(new int[0]));
+        assertEquals("Should work for 1-element arrays", 1, kata15.mostFrequentItemCount(new int[] {9}));
+        assertEquals("Should work with multiple most frequent items, e.g. nine 7's and nine 1's", 3, kata15.mostFrequentItemCount(new int[] {7, 1, 7, 1, 7, 1}));
+    }
 
 }
