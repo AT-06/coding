8a747f7be6a76d5b46971d919dc8c31be1a6e833
pipo411 <Ariel.Gonzales@fundacion-jala.org>
2018-03-18 23:47:31 -0400

    Kata9 Solved

---
+++ src/main/java/org/fundacionjala/coding/ariel/movies/Movie.java
@@ -0,0 +1,41 @@
+package org.fundacionjala.coding.ariel.movies;
+
+/**
+ * Class Movie.
+ */
+public abstract class Movie {
+    protected final String title;
+
+    /**
+     * Init.
+     *
+     * @param titleMovie of the Movie.
+     */
+    public Movie(String titleMovie) {
+        this.title = titleMovie;
+    }
+
+    /**
+     * @param daysRented is the quantity of rental days.
+     * @return the total amount of the Movie type.
+     */
+    abstract double calculateAmount(int daysRented);
+
+    /**
+     * @param daysRented is the quantity of rental days.
+     * @return the total frequent renter points of the Movie type.
+     */
+    abstract int calculateFrequentRenterPoints(int daysRented);
+
+    /**
+     * @param daysRented is the quantity of rental days.
+     * @return the rental figure depending of the Movie type.
+     */
+    public String rentalFigure(int daysRented) {
+        StringBuilder result = new StringBuilder();
+        result.append(title)
+                .append('\t')
+                .append(calculateAmount(daysRented));
+        return result.toString();
+    }
+}
