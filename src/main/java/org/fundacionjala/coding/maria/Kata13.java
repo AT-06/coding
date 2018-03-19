c863cdfc47ce63432776a9c02dca6142f3c461eb
mery <mariarenee.canqui@gmail.com>
2018-03-19 13:06:18 -0400

    Maria kata13 (#113)
    * kata 13 solution
    
    * kata 13 solution
    
    * kata 13 added test case
    
    * kata 13 changes requested
    
    * kata 13 changes
    
    * kata 13 changes 2
    
    * kata 13 test added

---
+++ src/main/java/org/fundacionjala/coding/maria/Kata13.java
@@ -0,0 +1,23 @@
+package org.fundacionjala.coding.maria;
+
+/**
+ * Kata 13, validate number.
+ */
+public class Kata13 {
+    private static final int TEN = 10;
+    private static final int THREE = 3;
+    /**
+     * @param number string number to validate
+     * @return boolean if the number is valid
+     */
+    public boolean validate(String number) {
+        String[] numberArray = number.split("");
+        int total = 0;
+        for (int i = 0; i < numberArray.length - 1; i += 2) {
+            total += Integer.parseInt(numberArray[i]);
+            total += Integer.parseInt(numberArray[i + 1]) * THREE;
+        }
+        total = total % TEN == 0 ? 0 : TEN - (total % TEN);
+        return total == Integer.parseInt(numberArray[numberArray.length - 1]);
+    }
+}
