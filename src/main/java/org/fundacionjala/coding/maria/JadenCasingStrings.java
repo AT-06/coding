2be1a2d1f352cb18cd856e87986cedc1edb8140e
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-05-03 20:47:29 -0400

    changes

--- src/main/java/org/fundacionjala/coding/maria/JadenCasingStrings.java
+++ src/main/java/org/fundacionjala/coding/maria/JadenCasingStrings.java
@@ -2,12 +2,16 @@ package org.fundacionjala.coding.maria;
 
 import java.util.StringJoiner;
 /**
- * Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
+ * Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013).
+ * Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter,
+ * he is known for almost always capitalizing every word.
  *
- * Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+ * Your task is to convert strings to how they would be written by Jaden Smith.
+ * The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way
+ * he originally typed them.
  *
  * */
-public class Jaden_Casing_Strings {
+public class JadenCasingStrings {
     /**
      * Method to convert a string on jaden case.
      * @param phrase is the phrase
@@ -15,7 +19,7 @@ public class Jaden_Casing_Strings {
      **/
 
     public String toJadenCase(String phrase) {
-        if (phrase != "" && phrase != null) {
+        if (phrase != null && phrase.compareTo("") != 0) {
             String[] phraseArray = phrase.split(" ");
             StringJoiner jaden = new StringJoiner(" ");
             for (String value : phraseArray) {
