8ac4c55cf2b854ed7fed3293defa12359ea10455
manu863018 <manuel.valdez@fundacion-jala.org>
2018-05-04 00:26:45 +0000

    Write Number in Expanded Form - Kata

--- src/main/java/org/fundacionjala/coding/manuel/Kata16.java
+++ src/main/java/org/fundacionjala/coding/manuel/Kata16.java
@@ -1,4 +1,38 @@
+/**
+ * Kata -> Write Number in Expanded Form
+ * You will be given a number and you will need to return it as a string in Expanded Form. For example:
+ *  Kata.expandedForm(12); # Should return "10 + 2"
+ *  Kata.expandedForm(42); # Should return "40 + 2"
+ *  Kata.expandedForm(70304); # Should return "70000 + 300 + 4"
+ * NOTE: All numbers will be whole numbers greater than 0.
+ */
 package org.fundacionjala.coding.manuel;
 
+/**
+ * Kata16.java
+ * Class for kata 16.
+ */
 public class Kata16 {
+    public static final int DELIMITER = 10;
+
+    /**
+     * Method to expand a number into its digits and units.
+     * @param num is the number to expand.
+     * @return is the string with number expanded.
+     */
+    public String expandedForm(int num) {
+        if (num <= DELIMITER) {
+            return Integer.toString(num);
+        }
+        int divisor = DELIMITER;
+        String result = "";
+        while (divisor < num) {
+            if ((num % divisor) > 0) {
+                result = " + ".concat(Integer.toString(num % divisor)).concat(result);
+            }
+            num -= num % divisor;
+            divisor *= DELIMITER;
+        }
+        return Integer.toString(num % divisor).concat(result);
+    }
 }
--- src/test/java/org/fundacionjala/coding/manuel/Kata16Test.java
+++ src/test/java/org/fundacionjala/coding/manuel/Kata16Test.java
@@ -1,5 +1,37 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.manuel;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Kata16Test.java class for unit tests.
+ */
 public class Kata16Test {
+    private Kata16 kata16;
 
+    /**
+     * Method to initialize object.
+     */
+    @Before
+    public void init() {
+        kata16 = new Kata16();
+    }
+
+    /**
+     * Test method expanded Form.
+     */
+    @Test
+    public void testExpandedForm() {
+        final int firstExpected = 12;
+        final int secondExpected = 42;
+        final int thirdExpected = 70304;
+        final int fourthExpected = 1234;
+        final int fifthExpected = 10;
+        assertEquals("10 + 2", kata16.expandedForm(firstExpected));
+        assertEquals("40 + 2", kata16.expandedForm(secondExpected));
+        assertEquals("70000 + 300 + 4", kata16.expandedForm(thirdExpected));
+        assertEquals("1000 + 200 + 30 + 4", kata16.expandedForm(fourthExpected));
+        assertEquals("10", kata16.expandedForm(fifthExpected));
+    }
 }
