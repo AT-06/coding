d67622ce08658c87c8cf5707bf26429031bb854c
manu863018 <manuel.valdez@fundacion-jala.org>
2018-05-04 23:25:00 +0000

    Sort the odd - Kata

--- src/main/java/org/fundacionjala/coding/manuel/Kata17.java
+++ src/main/java/org/fundacionjala/coding/manuel/Kata17.java
@@ -1,4 +1,43 @@
+/**
+ * Sort the odd
+ *
+ * You have an array of numbers.
+ * Your task is to sort ascending odd numbers but even numbers must be on their places.
+ * Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+ * Example
+ *  sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+ */
 package org.fundacionjala.coding.manuel;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+/**
+ * Kata17.java
+ * Class for kata 17.
+ */
 public class Kata17 {
+    /**
+     * Method to sort arrays only odd numbers.
+     * @param array to sort.
+     * @return array sorted.
+     */
+    public int[] sortArray(int[] array) {
+        if (array == null || array.length < 1) {
+            return array;
+        }
+        List<Integer> oddArray = new ArrayList<>();
+        List<Integer> oddPosition = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                oddArray.add(array[i]);
+                oddPosition.add(i);
+            }
+        }
+        Collections.sort(oddArray);
+        for (int j = 0; j < oddArray.size(); j++) {
+            array[oddPosition.get(j)] = oddArray.get(j);
+        }
+        return array;
+    }
 }
--- src/test/java/org/fundacionjala/coding/manuel/Kata17Test.java
+++ src/test/java/org/fundacionjala/coding/manuel/Kata17Test.java
@@ -1,5 +1,49 @@
-import static org.junit.Assert.*;
 
+package org.fundacionjala.coding.manuel;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Kata17Test.java class for unit tests.
+ */
 public class Kata17Test {
+    private Kata17 kata17;
 
+    /**
+     * Method to initialize object.
+     */
+    @Before
+    public void init() {
+        kata17 = new Kata17();
+    }
+
+    /**
+     * Test sortArray method, filled with numbers.
+     */
+    @Test
+    public void testSortArrayOrderWithNumbers() {
+        final int[] expected = new int[]{1, 3, 2, 8, 5, 4};
+        final int[] actual = new int[]{5, 3, 2, 8, 1, 4};
+        assertArrayEquals(expected, kata17.sortArray(actual));
+    }
+
+    /**
+     * Test sortArray method, filled with numbers and ceros.
+     */
+    @Test
+    public void testSortArrayOrderWithNumbersAndZeros() {
+        final int[] expected = new int[]{1, 3, 5, 8, 0};
+        final int[] actual = new int[]{5, 3, 1, 8, 0};
+        assertArrayEquals(expected, kata17.sortArray(actual));
+    }
+
+    /**
+     * Test sortArray method, empty array..
+     */
+    @Test
+    public void testSortArrayOrderEmptyArray() {
+        assertArrayEquals(new int[]{}, kata17.sortArray(new int[]{}));
+    }
 }
