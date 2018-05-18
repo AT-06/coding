bfec22ee8fc8790e6a5ab0ca3092bdd1c1e64ad1
mrcm15 <mariarenee.canqui@gmail.com>
2018-05-18 03:52:31 -0400

    commit 2

--- src/main/java/org/fundacionjala/coding/maria/WhichAreIn.java
+++ src/main/java/org/fundacionjala/coding/maria/WhichAreIn.java
@@ -1,4 +1,36 @@
 package org.fundacionjala.coding.maria;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order.
+ * of the strings of a1 which are substrings of strings of a2.
+ #Example 1: a1 = ["arp", "live", "strong"].
+ a2 = ["lively", "alive", "harp", "sharp", "armstrong"].
+ returns ["arp", "live", "strong"].
+ #Example 2: a1 = ["tarp", "mice", "bull"].
+ a2 = ["lively", "alive", "harp", "sharp", "armstrong"].
+ returns [].
+ */
 public class WhichAreIn {
+    /**
+     * inArray verify if the string contains elements of another string.
+     * @param array1 strings
+     * @param array2 strings
+     * @return array of strings
+     */
+    public String[] inArray(String[] array1, String[] array2) {
+        ArrayList<String> result = new ArrayList<String>();
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array2[j].contains(array1[i])) {
+                    result.add(array1[i]);
+                    break;
+                }
+            }
+        }
+        Collections.sort(result);
+        return result.toArray(new String[result.size()]);
+    }
 }
--- src/test/java/org/fundacionjala/coding/maria/WhichAreInTest.java
+++ src/test/java/org/fundacionjala/coding/maria/WhichAreInTest.java
@@ -1,5 +1,31 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.maria;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * test for kata WhichAreIn.
+ */
 public class WhichAreInTest {
+    private WhichAreIn whichAreIn;
+    /**
+     * initialize WhichAreIn.
+     */
+    @Before
+    public void initialize() {
+        whichAreIn = new WhichAreIn();
+    }
+    /**
+     * test basic test.
+     */
+    @Test
+    public void test1() {
+        String[] a = new String[]{"arp", "live", "strong"};
+        String[] b = new String[] {"lively", "alive", "harp", "sharp", "armstrong"};
+        String[] r = new String[] {"arp", "live", "strong"};
+        assertArrayEquals(r, whichAreIn.inArray(a, b));
+    }
 
 }
