b46d643ded1e0cda7a25318abd2754b31c6e9a4d
mrcm15 <mariarenee.canqui@gmail.com>
2018-05-28 06:02:50 -0400

    2

--- src/main/java/org/fundacionjala/coding/maria/ConvertStringToCamelCase.java
+++ src/main/java/org/fundacionjala/coding/maria/ConvertStringToCamelCase.java
@@ -1,4 +1,26 @@
 package org.fundacionjala.coding.maria;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Complete the method/function so that it converts dash/underscore delimited words.
+ * into camel casing. The first word within the output should be capitalized only.
+ * if the original word was capitalized.
+ * toCamelCase("the-stealth-warrior"); // returns "theStealthWarrior"
+ */
 public class ConvertStringToCamelCase {
+    /**
+     * Method to resolve the kata.
+     * @param s is the String.
+     * @return the new String.
+     */
+    public String toCamelCase(String s) {
+        Matcher m = Pattern.compile("[_|-](\\w)").matcher(s);
+        StringBuffer buffer = new StringBuffer();
+        while (m.find()) {
+            m.appendReplacement(buffer, m.group(1).toUpperCase());
+        }
+        return m.appendTail(buffer).toString();
+    }
 }
--- src/test/java/org/fundacionjala/coding/maria/ConvertStringToCamelCaseTest.java
+++ src/test/java/org/fundacionjala/coding/maria/ConvertStringToCamelCaseTest.java
@@ -1,7 +1,68 @@
 package org.fundacionjala.coding.maria;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * test fot kata ConvertStringToCamelCase.
+ */
 public class ConvertStringToCamelCaseTest {
+    private ConvertStringToCamelCase solution;
+    /**
+     * initialize ConvertStringToCamelCase kata.
+     */
+    @Before
+    public void initialize() {
+        solution = new ConvertStringToCamelCase();
+    }
+
+    /**
+     * test without strings.
+     */
+    @Test
+    public void testEmptyString() {
+        String input = "";
+        System.out.println("input: " + input);
+        assertEquals("", solution.toCamelCase(input));
+    }
+    /**
+     * test with some words.
+     */
+    @Test
+    public void testSomeUnderscoreLowerStart() {
+        String input = "the_Stealth_Warrior";
+        System.out.println("input: " + input);
+        assertEquals("theStealthWarrior", solution.toCamelCase(input));
+    }
+    /**
+     * testSomeDashLowerStart.
+     */
+    @Test
+    public void testSomeDashLowerStart() {
+        String input = "the-Stealth-Warrior";
+        System.out.println("input: " + input);
+        assertEquals("theStealthWarrior", solution.toCamelCase(input));
+    }
+    /**
+     * testSomeUnderscoreUpperStart.
+     */
+    @Test
+    public void testSomeUnderscoreUpperStart() {
+        String input = "The_Stealth_Warrior";
+        System.out.println("input: " + input);
+        assertEquals("TheStealthWarrior", solution.toCamelCase(input));
+    }
+    /**
+     * testSomeUnderscoreUpperStart.
+     */
+    @Test
+    public void testSomeDashUpperStart() {
+        String input = "The-Stealth-Warrior";
+        System.out.println("input: " + input);
+        assertEquals("TheStealthWarrior", solution.toCamelCase(input));
+    }
 
 }
