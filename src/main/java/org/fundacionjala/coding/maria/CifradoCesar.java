a9449678728bb154b036db0c2a9cdb54a6d616e1
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-06-13 21:22:37 -0400

    kata cesar solution

--- src/main/java/org/fundacionjala/coding/maria/CifradoCesar.java
+++ src/main/java/org/fundacionjala/coding/maria/CifradoCesar.java
@@ -1,4 +1,84 @@
 package org.fundacionjala.coding.maria;
 
+
+/**
+ * kata cifrado cesar.
+ */
 public class CifradoCesar {
+    private static final int A_POSITION = 64;
+    private static final int Z_POSITION = 90;
+    private static final int ABC_SIZE = 26;
+    /**
+     * method encode to encode a string using a key.
+     * @param sentence string.
+     * @param key number.
+     * @return the encode string.
+     */
+    public String encode(String sentence, int key) {
+        if (sentence == null || sentence.length() == 0) {
+            return "";
+        }
+        String[] letters = sentence.split("");
+        char[] newSentence = new char[letters.length];
+        for (int i = 0; i < letters.length; i++) {
+            char aux = letters[i].charAt(0);
+            newSentence[i] = (char) getPosition((int) aux, key);
+        }
+        return String.valueOf(newSentence);
+    }
+
+    /**
+     * Method decode to decode an encode sentence.
+     * @param sentence String.
+     * @param key int.
+     * @return a new String decode.
+     */
+    public String decode(String sentence, int key) {
+        if (sentence == null || sentence.length() == 0) {
+            return "";
+        }
+        return encode(sentence, -key);
+    }
+
+    /**
+     * Method vigenere to use a key word.
+     * @param clave String.
+     * @param menssaje String.
+     * @return a new string.
+     */
+    public String vigenere(String clave, String menssaje) {
+        String[] lettersMessaje = menssaje.split("");
+        char[] newMessaje = new char[menssaje.length()];
+
+        String[] lettersClave = clave.split("");
+        int[] positionClave = new int[lettersClave.length];
+        for (int i = 0; i < lettersClave.length; i++) {
+            positionClave[i] = (int) lettersClave[i].charAt(0);
+        }
+        int j = 0;
+        for (int i = 0; i < lettersMessaje.length; i++) {
+            char ascii = lettersMessaje[i].charAt(0);
+            j = j >=  clave.length() ? 0 : j;
+            newMessaje[i] = (char) getPosition((int) ascii, positionClave[j] - A_POSITION);
+            if ((int) ascii  > A_POSITION && (int) ascii <= Z_POSITION) {
+                j++;
+            }
+        }
+        return String.valueOf(newMessaje);
+    }
+
+    /**
+     * Method getPosition to get the new ascii position.
+     * @param pos int position.
+     * @param key int key.
+     * @return a new int.
+     */
+    public int getPosition(int pos, int key) {
+        if (pos > A_POSITION && pos <= Z_POSITION) {
+            pos += key;
+            pos = pos > Z_POSITION ? pos - ABC_SIZE : pos;
+            pos = pos < A_POSITION + 1 ? pos + ABC_SIZE : pos;
+        }
+        return pos;
+    }
 }
--- src/test/java/org/fundacionjala/coding/maria/CifradoCesarTest.java
+++ src/test/java/org/fundacionjala/coding/maria/CifradoCesarTest.java
@@ -1,5 +1,85 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.maria;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * class cifrado cesar test.
+ */
 public class CifradoCesarTest {
+    private CifradoCesar cesar;
+    private static final int THREE = 3;
+    private static final int NEGONE = -1;
+    private static final int ONE = 1;
 
+    /**
+     * initialize cifrado cesar.
+     */
+    @Before
+    public void initialize() {
+        cesar = new CifradoCesar();
+    }
+
+    /**
+     * test to encode with positive key.
+     */
+    @Test
+    public void encodeWithPositive() {
+        String sentence = "KROD";
+        assertEquals(sentence, cesar.encode("HOLA", THREE));
+    }
+
+    /**
+     * test to encode with negative key.
+     */
+    @Test
+    public void encodeWithNegative() {
+        String sentence = "ZAB";
+        assertEquals(sentence, cesar.encode("ABC", NEGONE));
+    }
+
+    /**
+     * test to encode with null string.
+     */
+    @Test
+    public void encodeWithNull() {
+        assertEquals("", cesar.encode(null, ONE));
+    }
+
+    /**
+     * test to decode with a positive key.
+     */
+    @Test
+    public void decodeWithPositive() {
+        String sentence = "W";
+        assertEquals(sentence, cesar.decode("Z", THREE));
+    }
+
+    /**
+     * test to decode with a negative key.
+     */
+    @Test
+    public void decodeWithNegative() {
+        String sentence = "BCD";
+        assertEquals(sentence, cesar.decode("ABC", NEGONE));
+    }
+
+    /**
+     * test to test decode with null.
+     */
+    @Test
+    public void decodeWithNull() {
+        assertEquals("", cesar.decode(null, ONE));
+    }
+
+    /**
+     * test to vigenere method.
+     */
+    @Test
+    public void vigeneteOneWord() {
+        String sentence = "TXYP XMUNHCRC";
+        assertEquals(sentence, cesar.vigenere("LIMON", "HOLA JALASOFT"));
+    }
 }
