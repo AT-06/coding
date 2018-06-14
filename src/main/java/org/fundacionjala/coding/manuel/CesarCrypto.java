99e5a0345e03d373a6363dfe85dafae263d21b21
manu863018 <manuel.valdez@fundacion-jala.org>
2018-06-14 01:13:08 +0000

    Test Kata, Cesar encryption.

--- src/main/java/org/fundacionjala/coding/manuel/CesarCrypto.java
+++ src/main/java/org/fundacionjala/coding/manuel/CesarCrypto.java
@@ -1,4 +1,56 @@
 package org.fundacionjala.coding.manuel;
+import java.util.StringJoiner;
 
-public class CesarCrypto {
+/**
+ * CesarCrypto class.
+ */
+public final class CesarCrypto {
+
+    private static final int ASCII_LOWER = 64;
+    private static final int ASCII_UPPER = 90;
+    private static final int ASCII_UPPER_PLUS = 91;
+
+    /**
+     * Constructor.
+     */
+    private CesarCrypto() {
+    }
+
+    /**
+     * Method to encode a text using Cesar algorithm.
+     * @param text to encode.
+     * @param key movements to make.
+     * @return the text encoded.
+     */
+    public static String encode(String text, int key) {
+        if (text == null || text.length() == 0) {
+            return "";
+        }
+        StringJoiner rotateLetters = new StringJoiner(" ");
+        for (String word : text.split(" ")) {
+            rotateLetters.add(rotateLetters(word, key));
+        }
+        return rotateLetters.toString();
+    }
+
+    /**
+     * Method to rotate letters for each word of the text.
+     * @param word to rotate its letters.
+     * @param key the number of rotations to make.
+     * @return the letter rotated.
+     */
+    public static String rotateLetters(String word, int key) {
+        char[] letters = word.toCharArray();
+        StringBuilder encriptedWord = new StringBuilder();
+        for (char letter : letters) {
+            if ((int) letter > ASCII_LOWER && (int) letter < ASCII_UPPER_PLUS) {
+                int movements = (int) letter + key;
+                int asciiCode = movements > ASCII_UPPER ? (movements - ASCII_UPPER) + ASCII_LOWER  : movements;
+                encriptedWord.append((char) asciiCode);
+            } else {
+                encriptedWord.append(letter);
+            }
+        }
+        return encriptedWord.toString();
+    }
 }
--- src/test/java/org/fundacionjala/coding/manuel/CesarCryptoTest.java
+++ src/test/java/org/fundacionjala/coding/manuel/CesarCryptoTest.java
@@ -1,5 +1,42 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.manuel;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Class to test CesarCrypto.
+ */
 public class CesarCryptoTest {
 
+    /**
+     * Test encode method.
+     */
+    @Test
+    public void testEncode() {
+        final int firstKey = 3;
+        final int secondKey = 5;
+        assertEquals("KROD", CesarCrypto.encode("HOLA", firstKey));
+        assertEquals("ETWWT", CesarCrypto.encode("ZORRO", secondKey));
+    }
+
+
+    /**
+     * Test encode methods for null and empty values.
+     */
+    @Test
+    public void testEncodeNullAndEmpty() {
+        final int key = 0;
+        assertEquals("", CesarCrypto.encode(null, key));
+        assertEquals("", CesarCrypto.encode("", key));
+    }
+
+    /**
+     * Test encode method for special characters.
+     */
+    @Test
+    public void testEncodeSpecialCharacters() {
+        final int key = 3;
+        assertEquals("KR_OD", CesarCrypto.encode("HO_LA", key));
+    }
 }
