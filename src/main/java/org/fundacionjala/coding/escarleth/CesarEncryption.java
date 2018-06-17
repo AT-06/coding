2e9945c4fa861adbd477f4260e73af7379cec9f7
EscarlethFatima <Escarleth.Ledezma@fundacion-jala.org>
2018-06-17 14:50:22 -0800

    decode added

--- src/main/java/org/fundacionjala/coding/escarleth/CesarEncryption.java
+++ src/main/java/org/fundacionjala/coding/escarleth/CesarEncryption.java
@@ -14,16 +14,16 @@ public class CesarEncryption {
      */
     public String encode(String cadToEncode, int key) {
 
-        if (cadToEncode == null || cadToEncode.isEmpty()) {
-            return "";
+        if (!isInvalidWord(cadToEncode)) {
+            StringBuilder cad = new StringBuilder();
+            key = key % ALPHABET_LENGTH;
+            for (int i = 0; i < cadToEncode.length(); i++) {
+                char character = cadToEncode.charAt(i);
+                cad.append(existInAlphabet(character) ? getLetterEncode(character, key) : character);
+            }
+            return cad.toString();
         }
-        StringBuilder cad = new StringBuilder();
-        key = key % ALPHABET_LENGTH;
-        for (int i = 0; i < cadToEncode.length(); i++) {
-            char character = Character.toUpperCase(cadToEncode.charAt(i));
-            cad.append(existInAlphabet(character) ? getLetterEncode(character, key) : character);
-        }
-        return cad.toString();
+        return "";
 
     }
 
@@ -38,12 +38,50 @@ public class CesarEncryption {
     }
 
     /**
+     * @param cadToDecode word to decode.
+     * @param key         the position to decode the letters.
+     * @return aDecodeWord
+     */
+    public String decode(String cadToDecode, int key) {
+        if (!isInvalidWord(cadToDecode)) {
+            StringBuilder cad = new StringBuilder();
+            key = key % ALPHABET_LENGTH;
+            for (int i = 0; i < cadToDecode.length(); i++) {
+                char character = cadToDecode.charAt(i);
+                cad.append(existInAlphabet(character) ? getLetterDecode(character, key) : character);
+            }
+            return cad.toString();
+        }
+        return "";
+    }
+
+    /**
+     * @param letter letter to encode
+     * @param key    the position to encode the letter
+     * @return aEncodeLetter
+     */
+    public char getLetterDecode(char letter, int key) {
+
+        return (letter + key) < 'A' ? (char) (letter + key + ALPHABET_LENGTH) : (char) (letter + key);
+
+    }
+
+    /**
      * @param letter letter to verify if exist in alphabet.
      * @return boolean exists in alphabet.
      */
     public boolean existInAlphabet(char letter) {
         return letter >= 'A' && letter <= 'Z';
     }
+
+    /**
+     * @param word word to verify if is a invalid word.
+     * @return boolean is null or empty.
+     */
+    public boolean isInvalidWord(String word) {
+        return word == null || word.isEmpty();
+    }
+
 
 }
 
--- src/test/java/org/fundacionjala/coding/escarleth/CesarEncryptionTest.java
+++ src/test/java/org/fundacionjala/coding/escarleth/CesarEncryptionTest.java
@@ -27,8 +27,8 @@ public class CesarEncryptionTest {
      */
     @Test
     public void testToEncodeValidWord() {
-        assertEquals("RHAH", cesarEncryption.encode("kata", KEY_7));
-        assertEquals("KROD", cesarEncryption.encode("Hola", KEY_3));
+        assertEquals("RHAH", cesarEncryption.encode("KATA", KEY_7));
+        assertEquals("KROD", cesarEncryption.encode("HOLA", KEY_3));
         assertEquals("ETWWT", cesarEncryption.encode("ZORRO", KEY_5));
     }
 
@@ -47,7 +47,35 @@ public class CesarEncryptionTest {
     @Test
     public void testToEncodeWithSpecialCharacters() {
         assertEquals("[RO?", cesarEncryption.encode("[OL?", KEY_3));
-        assertEquals("E{ WT", cesarEncryption.encode("Z{ ro", KEY_5));
+        assertEquals("E{ WT", cesarEncryption.encode("Z{ RO", KEY_5));
+    }
+
+    /**
+     * This method is for testing the encoding of a word with valid characters.
+     */
+    @Test
+    public void testToDecodeValidWord() {
+        assertEquals("KATA", cesarEncryption.decode("RHAH", -KEY_7));
+        assertEquals("HOLA", cesarEncryption.decode("KROD", -KEY_3));
+        assertEquals("ZORRO", cesarEncryption.decode("ETWWT", -KEY_5));
+    }
+
+    /**
+     * This method is for testing the decoding of a word with special characters.
+     */
+    @Test
+    public void testToDecodeWithSpecialCharacters() {
+        assertEquals("[OL?", cesarEncryption.decode("[RO?", -KEY_3));
+        assertEquals("Z{ RO", cesarEncryption.decode("E{ WT", -KEY_5));
+    }
+
+    /**
+     * This method is for testing the decoding of a word with empty or null word.
+     */
+    @Test
+    public void testToDecodeEmptyWord() {
+        assertEquals("", cesarEncryption.decode(null, KEY_7));
+        assertEquals("", cesarEncryption.decode("", KEY_7));
     }
 }
 
